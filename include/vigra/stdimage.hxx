30cf17a729ffee51a4ae05aa54e2134e8cc8b7a7
Ullrich Koethe <koethe@informatik.uni-hamburg.de>
2001-04-20 15:29:31 +0200

    srcImage() etc. can noew handle const references

--- include/vigra/stdimage.hxx
+++ include/vigra/stdimage.hxx
@@ -533,6 +533,26 @@ srcImage(BasicImage<PixelType> & img, Accessor a)
 }
 
 template <class PixelType, class Accessor>
+inline triple<typename BasicImage<PixelType>::ConstIterator, 
+              typename BasicImage<PixelType>::ConstIterator, Accessor>
+srcImageRange(BasicImage<PixelType> const & img, Accessor a)
+{
+    return triple<typename BasicImage<PixelType>::ConstIterator, 
+                  typename BasicImage<PixelType>::ConstIterator, 
+          Accessor>(img.upperLeft(),
+                    img.lowerRight(),
+                a);
+}
+
+template <class PixelType, class Accessor>
+inline pair<typename BasicImage<PixelType>::ConstIterator, Accessor>
+srcImage(BasicImage<PixelType> const & img, Accessor a)
+{
+    return pair<typename BasicImage<PixelType>::ConstIterator, 
+                Accessor>(img.upperLeft(), a);
+}
+
+template <class PixelType, class Accessor>
 inline triple<typename BasicImage<PixelType>::Iterator, 
               typename BasicImage<PixelType>::Iterator, Accessor>
 destImageRange(BasicImage<PixelType> & img, Accessor a)
@@ -560,6 +580,14 @@ maskImage(BasicImage<PixelType> & img, Accessor a)
                 Accessor>(img.upperLeft(), a);
 }
 
+template <class PixelType, class Accessor>
+inline pair<typename BasicImage<PixelType>::ConstIterator, Accessor>
+maskImage(BasicImage<PixelType> const & img, Accessor a)
+{
+    return pair<typename BasicImage<PixelType>::ConstIterator, 
+                Accessor>(img.upperLeft(), a);
+}
+
 /****************************************************************/
 
 template <class PixelType>
@@ -581,6 +609,29 @@ inline pair< typename BasicImage<PixelType>::Iterator,
 srcImage(BasicImage<PixelType> & img)
 {
     return pair<typename BasicImage<PixelType>::Iterator, 
+                typename BasicImage<PixelType>::Accessor>(img.upperLeft(), 
+                                         img.accessor());
+}
+
+template <class PixelType>
+inline triple<typename BasicImage<PixelType>::ConstIterator, 
+              typename BasicImage<PixelType>::ConstIterator, 
+          typename BasicImage<PixelType>::Accessor>
+srcImageRange(BasicImage<PixelType> const & img)
+{
+    return triple<typename BasicImage<PixelType>::ConstIterator, 
+                  typename BasicImage<PixelType>::ConstIterator, 
+          typename BasicImage<PixelType>::Accessor>(img.upperLeft(),
+                                        img.lowerRight(),
+                        img.accessor());
+}
+
+template <class PixelType>
+inline pair< typename BasicImage<PixelType>::ConstIterator, 
+             typename BasicImage<PixelType>::Accessor>
+srcImage(BasicImage<PixelType> const & img)
+{
+    return pair<typename BasicImage<PixelType>::ConstIterator, 
                 typename BasicImage<PixelType>::Accessor>(img.upperLeft(), 
                                          img.accessor());
 }
@@ -614,6 +665,16 @@ inline pair< typename BasicImage<PixelType>::Iterator,
 maskImage(BasicImage<PixelType> & img)
 {
     return pair<typename BasicImage<PixelType>::Iterator, 
+                typename BasicImage<PixelType>::Accessor>(img.upperLeft(), 
+                                         img.accessor());
+}
+
+template <class PixelType>
+inline pair< typename BasicImage<PixelType>::ConstIterator, 
+             typename BasicImage<PixelType>::Accessor>
+maskImage(BasicImage<PixelType> const & img)
+{
+    return pair<typename BasicImage<PixelType>::ConstIterator, 
                 typename BasicImage<PixelType>::Accessor>(img.upperLeft(), 
                                          img.accessor());
 }
