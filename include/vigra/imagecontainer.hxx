dadc3e692198dfc0923f2e3645b82dfb8c1afc28
Ullrich Koethe <koethe@informatik.uni-hamburg.de>
2002-11-08 00:15:55 +0100

    added copyright

--- include/vigra/imagecontainer.hxx
+++ include/vigra/imagecontainer.hxx
@@ -1,4 +1,25 @@
-#ifndef IMAGECONTAINER_HXX // -*- indent-tabs-mode: nil -*-
+/************************************************************************/
+/*                                                                      */
+/*               Copyright 1998-2002 by Ullrich Koethe                  */
+/*       Cognitive Systems Group, University of Hamburg, Germany        */
+/*                                                                      */
+/*    This file is part of the VIGRA computer vision library.           */
+/*    You may use, modify, and distribute this software according       */
+/*    to the terms stated in the LICENSE file included in               */
+/*    the VIGRA distribution.                                           */
+/*                                                                      */
+/*    The VIGRA Website is                                              */
+/*        http://kogs-www.informatik.uni-hamburg.de/~koethe/vigra/      */
+/*    Please direct questions, bug reports, and contributions to        */
+/*        koethe@informatik.uni-hamburg.de                              */
+/*                                                                      */
+/*  THIS SOFTWARE IS PROVIDED AS IS AND WITHOUT ANY EXPRESS OR          */
+/*  IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED      */
+/*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. */
+/*                                                                      */
+/************************************************************************/
+ 
+#ifndef IMAGECONTAINER_HXX 
 #define IMAGECONTAINER_HXX
 
 #include "vigra/utilities.hxx"
