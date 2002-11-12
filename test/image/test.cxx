2892e65d7ff43f6f7c062d58255957aa238461e9
Ullrich Koethe <koethe@informatik.uni-hamburg.de>
2002-11-13 00:29:59 +0100

    removed redundant impex tests (are now in directory test/impex)

--- test/image/test.cxx
+++ test/image/test.cxx
@@ -2,7 +2,6 @@
 #include "unittest.hxx"
 #include "vigra/stdimage.hxx"
 #include "vigra/imageiterator.hxx"
-#include "vigra/viff.hxx"
 #include "vigra/impex.hxx"
 #include "vigra/imagecontainer.hxx"
 #include "vigra/inspectimage.hxx"
@@ -20,7 +19,7 @@ struct ImageTest
     : img(3,3)
     {
         typename Image::Accessor acc = img.accessor();
-        typename Image::ScanOrderIterator i = img.begin();
+        typename Image::iterator i = img.begin();
 
         acc.set(data[0], i);
         ++i;
@@ -157,7 +156,7 @@ struct ImageTest
                  (ul+Diff2D(2,0)).columnIterator(), img.height());
 
         typename Image::Accessor acc = img.accessor();
-        typename Image::ScanOrderIterator i = img.begin();
+        typename Image::iterator i = img.begin();
         should(acc(i, 4) == data[4]);
     }
 
@@ -177,8 +176,8 @@ struct ImageTest
         typedef typename Image::value_type Value;
 
         Image img1(img);
-        typename Image::ScanOrderIterator i = img.begin();
-        typename Image::ScanOrderIterator i1 = img1.begin();
+        typename Image::iterator i = img.begin();
+        typename Image::iterator i1 = img1.begin();
         typename Image::Accessor acc = img.accessor();
 
         for(; i != img.end(); ++i, ++i1)
@@ -241,209 +240,6 @@ FRGB FRGBImageTest::data[] = {
     FRGB(9.9, 9.9, 9.9)
 };
 
-struct BImageImpexTest
-: public BImageTest
-{
-    void storeLoadViffImage()
-    {
-        ViffImage * viff = createViffImage(srcImageRange(img));
-        writeViffImage("test.xv", viff);
-
-        freeViffImage(viff);
-        viff = 0;
-
-        viff = readViffImage("test.xv");
-
-        should(viff != 0);
-        should(viff->row_size == 3);
-        should(viff->col_size == 3);
-        should(viff->num_data_bands == 1);
-        should(viff->data_storage_type == VFF_TYP_1_BYTE);
-
-        Image img1(3,3);
-
-        importViffImage(viff, destImage(img1));
-        freeViffImage(viff);
-
-        Image::ScanOrderIterator i = img.begin();
-        Image::ScanOrderIterator i1 = img1.begin();
-        Image::Accessor acc = img.accessor();
-
-        for(; i != img.end(); ++i, ++i1)
-        {
-            should(acc(i) == acc(i1));
-        }
-    }
-
-    void storeLoadGifImage()
-    {
-        exportImage(srcImageRange(img), vigra::ImageExportInfo("test.gif"));
-
-        vigra::ImageImportInfo info("test.gif");
-
-        should(info.isGrayscale());
-                should(info.width() == 3);
-        should(info.height() == 3);
-
-        Image img1(3,3);
-
-        importImage(info, destImage(img1));
-
-        Image::ScanOrderIterator i = img.begin();
-        Image::ScanOrderIterator i1 = img1.begin();
-        Image::Accessor acc = img.accessor();
-
-        for(; i != img.end(); ++i, ++i1)
-        {
-            should(acc(i) == acc(i1));
-        }
-    }
-};
-
-struct DImageImpexTest
-: public DImageTest
-{
-    void storeLoadImage()
-    {
-        ViffImage * viff = createViffImage(srcImageRange(img));
-        writeViffImage("test.xv", viff);
-
-        freeViffImage(viff);
-        viff = 0;
-
-        viff = readViffImage("test.xv");
-
-        should(viff != 0);
-        should(viff->row_size == 3);
-        should(viff->col_size == 3);
-        should(viff->num_data_bands == 1);
-        should(viff->data_storage_type == VFF_TYP_DOUBLE);
-
-        Image img1(3,3);
-
-        importViffImage(viff, destImage(img1));
-        freeViffImage(viff);
-
-        Image::ScanOrderIterator i = img.begin();
-        Image::ScanOrderIterator i1 = img1.begin();
-        Image::Accessor acc = img.accessor();
-
-        for(; i != img.end(); ++i, ++i1)
-        {
-            should(acc(i) == acc(i1));
-        }
-    }
-};
-
-struct BRGBImageImpexTest
-: public BRGBImageTest
-{
-    void storeLoadViffImage()
-    {
-        ViffImage * viff = createViffImage(srcImageRange(img));
-        writeViffImage("test.xv", viff);
-
-        freeViffImage(viff);
-        viff = 0;
-
-        viff = readViffImage("test.xv");
-
-        should(viff != 0);
-        should(viff->row_size == 3);
-        should(viff->col_size == 3);
-        should(viff->num_data_bands == 3);
-        should(viff->data_storage_type == VFF_TYP_1_BYTE);
-
-        Image img1(3,3);
-
-        importViffImage(viff, destImage(img1));
-        freeViffImage(viff);
-
-        Image::ScanOrderIterator i = img.begin();
-        Image::ScanOrderIterator i1 = img1.begin();
-        Image::Accessor acc = img.accessor();
-
-        for(; i != img.end(); ++i, ++i1)
-        {
-            should(acc(i) == acc(i1));
-        }
-    }
-
-    void storeLoadGifImage()
-    {
-        vigra::ImageImportInfo info("lenna.gif");
-
-        should(info.isColor());
-
-        Image img1(info.width(), info.height());
-
-        importImage(info, destImage(img1));
-
-        exportImage(srcImageRange(img1), vigra::ImageExportInfo("test.ras"));
-
-        ViffImage * viff = readViffImage("lenna.xv");
-        should(viff != 0);
-
-        vigra::ImageImportInfo info1("test.ras");
-
-        should(info1.isColor());
-        should(info1.width() == (int)viff->row_size);
-        should(info1.height() == (int)viff->col_size);
-        should(info1.width() == info.width());
-        should(info1.height() == info.height());
-
-        Image img2(info1.width(), info1.height());
-
-        importImage(info1, destImage(img1));
-
-        importViffImage(viff, destImage(img2));
-        freeViffImage(viff);
-
-        Image::ScanOrderIterator i1 = img1.begin();
-        Image::ScanOrderIterator i2 = img2.begin();
-        Image::Accessor acc = img1.accessor();
-
-        for(; i1 != img1.end(); ++i1, ++i2)
-        {
-            should(acc(i1) == acc(i2));
-        }
-    }
-};
-
-struct FRGBImageImpexTest
-: public FRGBImageTest
-{
-    void storeLoadImage()
-    {
-        ViffImage * viff = createViffImage(srcImageRange(img));
-        writeViffImage("test.xv", viff);
-
-        freeViffImage(viff);
-        viff = 0;
-
-        viff = readViffImage("test.xv");
-
-        should(viff != 0);
-        should(viff->row_size == 3);
-        should(viff->col_size == 3);
-        should(viff->num_data_bands == 3);
-        should(viff->data_storage_type == VFF_TYP_FLOAT);
-
-        Image img1(3,3);
-
-        importViffImage(viff, destImage(img1));
-        freeViffImage(viff);
-
-        Image::ScanOrderIterator i = img.begin();
-        Image::ScanOrderIterator i1 = img1.begin();
-        Image::Accessor acc = img.accessor();
-
-        for(; i != img.end(); ++i, ++i1)
-        {
-            should(acc(i) == acc(i1));
-        }
-    }
-};
 
 struct ImageTestSuite
 : public vigra::test_suite
@@ -454,21 +250,15 @@ struct ImageTestSuite
         add( testCase( &BImageTest::testBasicImageIterator));
         add( testCase( &BImageTest::testImageIterator));
         add( testCase( &BImageTest::copyImage));
-        add( testCase( &BImageImpexTest::storeLoadViffImage));
-        add( testCase( &BImageImpexTest::storeLoadGifImage));
         add( testCase( &DImageTest::testBasicImageIterator));
         add( testCase( &DImageTest::testImageIterator));
         add( testCase( &DImageTest::copyImage));
-        add( testCase( &DImageImpexTest::storeLoadImage));
         add( testCase( &BRGBImageTest::testBasicImageIterator));
         add( testCase( &BRGBImageTest::testImageIterator));
         add( testCase( &BRGBImageTest::copyImage));
-        add( testCase( &BRGBImageImpexTest::storeLoadViffImage));
-        add( testCase( &BRGBImageImpexTest::storeLoadGifImage));
         add( testCase( &FRGBImageTest::testBasicImageIterator));
         add( testCase( &FRGBImageTest::testImageIterator));
         add( testCase( &FRGBImageTest::copyImage));
-        add( testCase( &FRGBImageImpexTest::storeLoadImage));
     }
 };
 
