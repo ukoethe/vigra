0d83de3bb9e544bed7779bf21550cd37a5e26870
Hans Meine <meine@informatik.uni-hamburg.de>
2003-01-20 14:41:55 +0100

    -#if defined(__GNUC__) && !defined(__SGI_STL_INTERNAL_RELOPS) +#if defined(__GNUC__) && (__GNUC__ < 3) && !defined(__SGI_STL_INTERNAL_RELOPS)

--- test/imagehierarchy/g++_relops_workaround.hxx
+++ test/imagehierarchy/g++_relops_workaround.hxx
@@ -1,13 +1,13 @@
-#if defined(__GNUC__) && !defined(__SGI_STL_INTERNAL_RELOPS)
+#if defined(__GNUC__) && (__GNUC__ < 3) && !defined(__SGI_STL_INTERNAL_RELOPS)
 
 #   define __STL_BEGIN_RELOPS_NAMESPACE namespace std { namespace rel_ops {
 #   define __STL_END_RELOPS_NAMESPACE }}
 #   define __STD_RELOPS std::rel_ops
- 
+
 #include <stl_relops.h>
 
-#   undef __STL_BEGIN_RELOPS_NAMESPACE 
-#   undef __STL_END_RELOPS_NAMESPACE 
+#   undef __STL_BEGIN_RELOPS_NAMESPACE
+#   undef __STL_END_RELOPS_NAMESPACE
 #   undef __STD_RELOPS
 #endif
 
