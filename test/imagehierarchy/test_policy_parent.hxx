7677aeeeab568cdea859e5ce803de6457719afe6
Hans Meine <meine@informatik.uni-hamburg.de>
2003-01-20 14:41:19 +0100

    GCC3 barked on this: --- test_policy_parent.hxx      2002/09/11 21:49:41     1.2 +++ test_policy_parent.hxx      2002/11/28 16:15:08 @@ -61,7 +61,7 @@ public:      {          return new ChildImage(image);      } -#endif IMAGEHIERARCHY +#endif // IMAGEHIERARCHY
     };// end of class TestPolicy

--- test/imagehierarchy/test_policy_parent.hxx
+++ test/imagehierarchy/test_policy_parent.hxx
@@ -6,9 +6,9 @@
 /** Ist eine Oberklasse aller ImagePolicys. ImagePolicys stellen die Besonderheiten der einzelner
 * Bilder dar, z.B. Vector2ImagePolicy stellt die Besonderheiten der Bilder Vector2Image, DVector2Image FVector2Image,
 * bei denen die Pixel TinyVectoren sind. Zusaetzlich wird der Vektor von entsprechenden Pixeln zu Verfuegung
-* gestellt, so dass die entsprechende Pixeln brauchen nicht in der Testklasse ImageTest erzeugt zu 
-* werden. Da aber die Methoden factory() fast in allen Policy - Klassen gleich sind wurde deswegen die 
-* abstrakte Oberklasse geschaffen, damit die Methoden brauchen nicht mehrmals auf gleiche Weise in 
+* gestellt, so dass die entsprechende Pixeln brauchen nicht in der Testklasse ImageTest erzeugt zu
+* werden. Da aber die Methoden factory() fast in allen Policy - Klassen gleich sind wurde deswegen die
+* abstrakte Oberklasse geschaffen, damit die Methoden brauchen nicht mehrmals auf gleiche Weise in
 * unterschiedlichen Unterklassen wiederhollt zu werden.
 */
 template<class ToTestImage>
@@ -22,25 +22,25 @@ public:
     typedef typename Image::value_type           child_value_type;
     typedef std::vector<value_type>              data_array_type;
     typedef std::vector<value_type>              child_data_array_type;
-    
+
     /** factory() - Methoden sind dazu, da um den Aufruf des richtigen Konstruktors
     * zu kapseln. Insbesondere geht es darum, dass fuer Tests von SingleBandImage
     * und VariableBandsImage die Konstruktoren der Unterklassen aufgerufen werden
     * muessen, da sie selbst abstrakte Klassen sind, also man kann nicht die Instanzen
-    * von Ihnen erzeugen. Bei allen anderen Bildern muessen aber die Konstruktoren 
+    * von Ihnen erzeugen. Bei allen anderen Bildern muessen aber die Konstruktoren
     * der Testklasse selber aufgerufen werden. Aus diesem Grunde auch die Unterscheidung
-    * zwischen Image und ChildImage. 
+    * zwischen Image und ChildImage.
     */
     static ChildImage * factory()
     {
         return new ChildImage();
     }
-    
+
     static ChildImage * factory(int x, int y)
     {
         return new ChildImage(x,y);
-    }     
-    
+    }
+
     static ChildImage * factory(vigra::Diff2D size)
     {
         return new ChildImage(size);
@@ -50,18 +50,18 @@ public:
     {
         return new ChildImage(x, y, pixel);
     }
-    
+
     static ChildImage * factory(ChildImage image)
     {
         return new ChildImage(image);
     }
 
-#ifdef IMAGEHIERARCHY    
+#ifdef IMAGEHIERARCHY
     static ChildImage * factory(typename ChildImage::InnerImage image)
     {
         return new ChildImage(image);
     }
-#endif IMAGEHIERARCHY     
+#endif // IMAGEHIERARCHY
 
 };// end of class TestPolicy
 
