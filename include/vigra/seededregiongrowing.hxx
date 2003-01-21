d1bf42fa4eac47f301c956fd5b583928f84931b6
Ullrich Koethe <koethe@informatik.uni-hamburg.de>
2003-01-21 22:38:35 +0100

    added seeded region growing option "KeepContour/CompleteGrow"

--- include/vigra/seededregiongrowing.hxx
+++ include/vigra/seededregiongrowing.hxx
@@ -18,8 +18,7 @@
 /*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. */
 /*                                                                      */
 /************************************************************************/
- 
- 
+  
 #ifndef VIGRA_SEEDEDREGIONGROWING_HXX
 #define VIGRA_SEEDEDREGIONGROWING_HXX
 
@@ -46,9 +45,9 @@ public:
     : location_(0,0), nearest_(0,0), cost_(0), count_(0), label_(0)
     {}
 
-    InternalSeedRgPixel(Diff2D const & location, Diff2D const & nearest, 
+    InternalSeedRgPixel(Diff2D const & location, Diff2D const & nearest,
                         COST const & cost, int const & count, int const & label)
-    : location_(location), nearest_(nearest), 
+    : location_(location), nearest_(nearest),
       cost_(cost), count_(count), label_(label)
     {
         int dx = location_.x - nearest_.x;
@@ -56,7 +55,7 @@ public:
         dist_ = dx * dx + dy * dy;
     }
 
-    void set(Diff2D const & location, Diff2D const & nearest, 
+    void set(Diff2D const & location, Diff2D const & nearest,
                         COST const & cost, int const & count, int const & label)
     {
         location_ = location;
@@ -73,7 +72,7 @@ public:
     struct Compare
     {
         // must implement > since priority_queue looks for largest element
-        bool operator()(InternalSeedRgPixel const & l, 
+        bool operator()(InternalSeedRgPixel const & l,
                         InternalSeedRgPixel const & r) const
         {
             if(r.cost_ == l.cost_)
@@ -85,7 +84,7 @@ public:
 
             return r.cost_ < l.cost_;
         }
-        bool operator()(InternalSeedRgPixel const * l, 
+        bool operator()(InternalSeedRgPixel const * l,
                         InternalSeedRgPixel const * r) const
         {
             if(r->cost_ == l->cost_)
@@ -110,8 +109,8 @@ public:
             }
         }
         
-        InternalSeedRgPixel * 
-        create(Diff2D const & location, Diff2D const & nearest, 
+        InternalSeedRgPixel *
+        create(Diff2D const & location, Diff2D const & nearest,
                COST const & cost, int const & count, int const & label)
         {
             if(!freelist_.empty())
@@ -134,10 +133,12 @@ public:
     };
 };
 
+enum SRGType { KeepContours, CompleteGrow };
+
 /** \addtogroup SeededRegionGrowing Seeded Region Growing
     Region segmentation and voronoi tesselation
 */
-//@{ 
+//@{
                                     
 /********************************************************/
 /*                                                      */
@@ -147,29 +148,29 @@ public:
 
 /** \brief Region Segmentation by means of Seeded Region Growing.
 
-    This algorithm implements seeded region growing as described in 
+    This algorithm implements seeded region growing as described in
     
     R. Adams, L. Bischof: "<em> Seeded Region Growing</em>", IEEE Trans. on Pattern
     Analysis and Maschine Intelligence, vol 16, no 6, 1994, and
     
-    Ullrich K&ouml;the: 
-    <em> "<a href="http://kogs-www.informatik.uni-hamburg.de/~koethe/papers/#primary">Primary Image Segmentation</a>"</em>, 
-    in: G. Sagerer, S. 
-    Posch, F. Kummert (eds.): Mustererkennung 1995, Proc. 17. DAGM-Symposium, 
+    Ullrich K&ouml;the:
+    <em> "<a href="http://kogs-www.informatik.uni-hamburg.de/~koethe/papers/#primary">Primary Image Segmentation</a>"</em>,
+    in: G. Sagerer, S.
+    Posch, F. Kummert (eds.): Mustererkennung 1995, Proc. 17. DAGM-Symposium,
     Springer 1995
     
     The seed image is a partly segmented image which contains uniquely
     labeled regions (the seeds) and unlabeled pixels (the candidates, label 0).
     Seed regions can be as large as you wish and as small as one pixel. If
     there are no candidates, the algorithm will simply copy the seed image
-    into the output image. Otherwise it will aggregate the candidates into 
-    the existing regions so that a cost function is minimized. This 
+    into the output image. Otherwise it will aggregate the candidates into
+    the existing regions so that a cost function is minimized. This
     works as follows:
     
     <ol>
     
     <li> Find all candidate pixels that are 4-adjacent to a seed region.
-    Calculate the cost for aggregating each candidate into its adajacent region 
+    Calculate the cost for aggregating each candidate into its adajacent region
     and put the candidates into a priority queue.
     
     <li> While( priority queue is not empty)
@@ -186,66 +187,71 @@ public:
     
     </ol>
     
-    This algorithm will always lead to a complete, 4-connected tesselation of
-    the image. 
+    If <tt>SRGType == CompleteGrow</tt> (the default), this algorithm 
+    will produce a complete 4-connected tesselation of the image.
+    If <tt>SRGType == KeepContours</tt>, a one-pixel-wide border will be left
+    between the regions. The border pixels get label -1.
     
     The cost is determined jointly by the source image and the
     region statistics functor. The source image contains feature values for each
-    pixel which will be used by the region statistics functor to calculate and 
+    pixel which will be used by the region statistics functor to calculate and
     update statistics for each region and to calculate the cost for each
-    candidate. The <TT>RegionStatisticsArray</TT> must be compatible to the 
-    \ref ArrayOfRegionStatistics functor and contains an <em> array</em> of 
+    candidate. The <TT>RegionStatisticsArray</TT> must be compatible to the
+    \ref ArrayOfRegionStatistics functor and contains an <em> array</em> of
     statistics objects for each region. The indices must correspond to the
     labels of the seed regions. The statistics for the initial regions must have
-    been calculated prior to calling <TT>seededRegionGrowing()</TT> (for example by 
-    means of \ref inspectTwoImagesIf()). 
+    been calculated prior to calling <TT>seededRegionGrowing()</TT> (for example by
+    means of \ref inspectTwoImagesIf()).
     
-    For each candidate 
-    <TT>x</TT> that is adjacent to region <TT>i</TT>, the algorithm will call 
-    <TT>stats[i].cost(as(x))</TT> to get the cost (where <TT>x</TT> is a <TT>SrcImageIterator</TT> 
-    and <TT>as</TT> is 
+    For each candidate
+    <TT>x</TT> that is adjacent to region <TT>i</TT>, the algorithm will call
+    <TT>stats[i].cost(as(x))</TT> to get the cost (where <TT>x</TT> is a <TT>SrcImageIterator</TT>
+    and <TT>as</TT> is
     the SrcAccessor). When a candidate has been merged with a region, the
     statistics are updated by calling <TT>stats[i].operator()(as(x))</TT>. Since
     the <TT>RegionStatisticsArray</TT> is passed by reference, this will overwrite
     the original statistics.
     
-    If a candidate could be merged into more than one regions with identical 
-    cost, the algorithm will favour the nearest region. 
+    If a candidate could be merged into more than one regions with identical
+    cost, the algorithm will favour the nearest region.
     
     In some cases, the cost only depends on the feature value of the current
     pixel. Then the update operation will simply be a no-op, and the <TT>cost()</TT>
-    function returns its argument. This behavior is implemented by the 
-    \ref SeedRgDirectValueFunctor.
+    function returns its argument. This behavior is implemented by the
+    \ref SeedRgDirectValueFunctor. With <tt>SRGType == KeepContours</tt>,
+    this is equivalent to the watershed algorithm.
     
     <b> Declarations:</b>
     
     pass arguments explicitly:
     \code
     namespace vigra {
-        template <class SrcImageIterator, class SrcAccessor, 
-                  class SeedImageIterator, class SeedAccessor,
-                  class DestImageIterator, class DestAccessor,
-	          class RegionStatisticsArray>
-        void seededRegionGrowing(SrcImageIterator srcul, 
-                                 SrcImageIterator srclr, SrcAccessor as, 
+       template <class SrcImageIterator, class SrcAccessor,
+                 class SeedImageIterator, class SeedAccessor,
+                 class DestImageIterator, class DestAccessor,
+                 class RegionStatisticsArray>
+        void seededRegionGrowing(SrcImageIterator srcul,
+                                 SrcImageIterator srclr, SrcAccessor as,
                                  SeedImageIterator seedsul, SeedAccessor aseeds,
-			         DestImageIterator destul, DestAccessor ad,
-		                 RegionStatisticsArray & stats);
+                                 DestImageIterator destul, DestAccessor ad,
+                                 RegionStatisticsArray & stats,
+                                 SRGType srgType = CompleteGrow);
     }
     \endcode
     
     use argument objects in conjuction with \ref ArgumentObjectFactories:
     \code
     namespace vigra {
-        template <class SrcImageIterator, class SrcAccessor, 
+        template <class SrcImageIterator, class SrcAccessor,
                   class SeedImageIterator, class SeedAccessor,
                   class DestImageIterator, class DestAccessor,
-	          class RegionStatisticsArray>
+                  class RegionStatisticsArray>
         inline void
         seededRegionGrowing(triple<SrcImageIterator, SrcImageIterator, SrcAccessor> img1,
-		           pair<SeedImageIterator, SeedAccessor> img3,
-		           pair<DestImageIterator, DestAccessor> img4,
-		           RegionStatisticsArray & stats);
+                            pair<SeedImageIterator, SeedAccessor> img3,
+                            pair<DestImageIterator, DestAccessor> img4,
+                            RegionStatisticsArray & stats,
+                            SRGType srgType = CompleteGrow);
     }
     \endcode
     
@@ -274,11 +280,11 @@ public:
     vigra::distanceTransform(srcImageRange(points), destImage(dist), 2);
     
     // init statistics functor
-    vigra::ArrayOfRegionStatistics<vigra::SeedRgDirectValueFunctor<float> > 
+    vigra::ArrayOfRegionStatistics<vigra::SeedRgDirectValueFunctor<float> >
                                               stats(max_region_label);
     
-    // find voronoi region of each point 
-   vigra:: seededRegionGrowing(srcImageRange(dist), srcImage(points), 
+    // find voronoi region of each point
+   vigra:: seededRegionGrowing(srcImageRange(dist), srcImage(points),
                                destImage(points), stats);
     \endcode
 
@@ -296,7 +302,7 @@ public:
     RegionStatisticsArray stats;
     
     // calculate costs
-    RegionStatisticsArray::value_type::cost_type cost = 
+    RegionStatisticsArray::value_type::cost_type cost =
         stats[seed_accessor(seed_upperleft)].cost(src_accessor(src_upperleft));
     
     // compare costs
@@ -309,17 +315,18 @@ public:
     dest_accessor.set(seed_accessor(seed_upperleft), dest_upperleft);
     \endcode
     
-    Further requirements are determined by the <TT>RegionStatisticsArray</TT>. 
+    Further requirements are determined by the <TT>RegionStatisticsArray</TT>.
 */
-template <class SrcImageIterator, class SrcAccessor, 
+template <class SrcImageIterator, class SrcAccessor,
           class SeedImageIterator, class SeedAccessor,
           class DestImageIterator, class DestAccessor,
-	  class RegionStatisticsArray>
-void seededRegionGrowing(SrcImageIterator srcul, 
-                         SrcImageIterator srclr, SrcAccessor as, 
+          class RegionStatisticsArray>
+void seededRegionGrowing(SrcImageIterator srcul,
+                         SrcImageIterator srclr, SrcAccessor as,
                          SeedImageIterator seedsul, SeedAccessor aseeds,
-			 DestImageIterator destul, DestAccessor ad,
-		         RegionStatisticsArray & stats)
+                         DestImageIterator destul, DestAccessor ad,
+                         RegionStatisticsArray & stats,
+                         const SRGType srgType)
 {
     int w = srclr.x - srcul.x;
     int h = srclr.y - srcul.y;
@@ -333,7 +340,7 @@ void seededRegionGrowing(SrcImageIterator srcul,
     
     typename Pixel::Allocator allocator;
     
-    typedef std::priority_queue<Pixel *, std::vector<Pixel *>, 
+    typedef std::priority_queue<Pixel *, std::vector<Pixel *>,
                                 typename Pixel::Compare>  SeedRgPixelHeap;
 
     // copy seed image in an image with border
@@ -347,21 +354,21 @@ void seededRegionGrowing(SrcImageIterator srcul,
     // allocate and init memory for the results
 
     SeedRgPixelHeap pheap;
+    int cneighbor;
 
-    static const Diff2D dist[] = { Diff2D(-1,0), Diff2D(0,-1), 
-                                 Diff2D(1,0),  Diff2D(0,1) };
+    static const Diff2D dist[] = { Diff2D(-1,0), Diff2D(0,-1),
+                                   Diff2D(1,0),  Diff2D(0,1) };
 
     Diff2D pos(0,0);
-    for(isy=srcul, iry=ir, pos.y=0; pos.y<h; 
-      ++pos.y, ++isy.y, ++iry.y)
+    for(isy=srcul, iry=ir, pos.y=0; pos.y<h;
+        ++pos.y, ++isy.y, ++iry.y)
     {
-        for(isx=isy, irx=iry, pos.x=0; pos.x<w; 
+        for(isx=isy, irx=iry, pos.x=0; pos.x<w;
             ++pos.x, ++isx.x, ++irx.x)
         {
             if(*irx == 0)
             {
                 // find candidate pixels for growing and fill heap
-                int cneighbor;
                 for(int i=0; i<4; i++)
                 {
                     cneighbor = irx[dist[i]];
@@ -369,7 +376,7 @@ void seededRegionGrowing(SrcImageIterator srcul,
                     {
                         CostType cost = stats[cneighbor].cost(as(isx));
 
-                        Pixel * pixel = 
+                        Pixel * pixel =
                             allocator.create(pos, pos+dist[i], cost, count++, cneighbor);
                         pheap.push(pixel);
                     }
@@ -378,7 +385,7 @@ void seededRegionGrowing(SrcImageIterator srcul,
         }
     }
 
-    // perform region growing  
+    // perform region growing
     while(pheap.size() != 0)
     {
         Pixel * pixel = pheap.top();
@@ -395,23 +402,39 @@ void seededRegionGrowing(SrcImageIterator srcul,
 
         if(*irx > 0) continue;
 
+        if(srgType == KeepContours)
+        {
+            for(int i=0; i<4; i++)
+            {
+                cneighbor = irx[dist[i]];
+                if((cneighbor>0) && (cneighbor != lab))
+                {
+                    lab = -1;
+                    break;
+                }
+            }
+        }
+
         *irx = lab;
 
-        // update statistics
-        stats[*irx](as(isx));
-
-        // search neighborhood
-        // second pass: find new candidate pixels
-        for(int i=0; i<4; i++)
+        if((srgType != KeepContours) || (lab > 0))
         {
-	    if(irx[dist[i]] == 0)
-	    {
-	        CostType cost = stats[lab].cost(as(isx, dist[i]));
+            // update statistics
+            stats[*irx](as(isx));
 
-	        Pixel * new_pixel = 
-                    allocator.create(pos+dist[i], nearest, cost, count++, lab);
-	        pheap.push(new_pixel);
-	    }
+            // search neighborhood
+            // second pass: find new candidate pixels
+            for(int i=0; i<4; i++)
+            {
+                if(irx[dist[i]] == 0)
+                {
+                    CostType cost = stats[lab].cost(as(isx, dist[i]));
+
+                    Pixel * new_pixel =
+                        allocator.create(pos+dist[i], nearest, cost, count++, lab);
+                    pheap.push(new_pixel);
+                }
+            }
         }
     }
 
@@ -419,20 +442,54 @@ void seededRegionGrowing(SrcImageIterator srcul,
     copyImage(ir, ir+Diff2D(w,h), regions.accessor(), destul, ad);
 }
 
-template <class SrcImageIterator, class SrcAccessor, 
+template <class SrcImageIterator, class SrcAccessor,
           class SeedImageIterator, class SeedAccessor,
           class DestImageIterator, class DestAccessor,
-	  class RegionStatisticsArray>
+          class RegionStatisticsArray>
+inline void 
+seededRegionGrowing(SrcImageIterator srcul,
+                    SrcImageIterator srclr, SrcAccessor as,
+                    SeedImageIterator seedsul, SeedAccessor aseeds,
+                    DestImageIterator destul, DestAccessor ad,
+                    RegionStatisticsArray & stats)
+{
+    seededRegionGrowing(img1.first, img1.second, img1.third,
+                        img3.first, img3.second,
+                        img4.first, img4.second,
+                        stats, CompleteGrow);
+}
+
+template <class SrcImageIterator, class SrcAccessor,
+          class SeedImageIterator, class SeedAccessor,
+          class DestImageIterator, class DestAccessor,
+          class RegionStatisticsArray>
 inline void
 seededRegionGrowing(triple<SrcImageIterator, SrcImageIterator, SrcAccessor> img1,
-		   pair<SeedImageIterator, SeedAccessor> img3,
-		   pair<DestImageIterator, DestAccessor> img4,
-		   RegionStatisticsArray & stats)
+                    pair<SeedImageIterator, SeedAccessor> img3,
+                    pair<DestImageIterator, DestAccessor> img4,
+                    RegionStatisticsArray & stats,
+                    SRGType srgType)
 {
-    seededRegionGrowing(img1.first, img1.second, img1.third, 
-                        img3.first, img3.second, 
-                        img4.first, img4.second, 
-			stats);
+    seededRegionGrowing(img1.first, img1.second, img1.third,
+                        img3.first, img3.second,
+                        img4.first, img4.second,
+                        stats, srgType);
+}
+
+template <class SrcImageIterator, class SrcAccessor,
+          class SeedImageIterator, class SeedAccessor,
+          class DestImageIterator, class DestAccessor,
+          class RegionStatisticsArray>
+inline void
+seededRegionGrowing(triple<SrcImageIterator, SrcImageIterator, SrcAccessor> img1,
+                    pair<SeedImageIterator, SeedAccessor> img3,
+                    pair<DestImageIterator, DestAccessor> img4,
+                    RegionStatisticsArray & stats)
+{
+    seededRegionGrowing(img1.first, img1.second, img1.third,
+                        img3.first, img3.second,
+                        img4.first, img4.second,
+                        stats, CompleteGrow);
 }
 
 /********************************************************/
@@ -443,9 +500,9 @@ seededRegionGrowing(triple<SrcImageIterator, SrcImageIterator, SrcAccessor> img1
 
 /** \brief Statistics functor to be used for seeded region growing.
 
-    This functor can be used if the cost of a candidate during 
+    This functor can be used if the cost of a candidate during
     \ref seededRegionGrowing() is equal to the feature value of that
-    candidate and does not depend on properties of the region it is going to 
+    candidate and does not depend on properties of the region it is going to
     be merged with.
 
     <b>\#include</b> "<a href="seededregiongrowing_8hxx-source.html">vigra/seededregiongrowing.hxx</a>"<br>
