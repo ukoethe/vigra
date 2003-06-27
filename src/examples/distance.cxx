061440ac24f826b77475d8526e7a0c3996876798
Ullrich Koethe <koethe@informatik.uni-hamburg.de>
2003-06-27 02:15:58 +0200

    initial

---
+++ src/examples/distance.cxx
@@ -0,0 +1,85 @@
+/************************************************************************/
+/*                                                                      */
+/*               Copyright 1998-2002 by Ullrich Koethe                  */
+/*       Cognitive Systems Group, University of Hamburg, Germany        */
+/*                                                                      */
+/*    This file is part of the VIGRA computer vision library.           */
+/*    You may use, modify, and distribute this software according       */
+/*    to the terms stated in the LICENSE file included in               */
+/*    the VIGRA distribution.                                           */
+/*                                                                      */
+/*    The VIGRA Website is                                              */
+/*        http://kogs-www.informatik.uni-hamburg.de/~koethe/vigra/      */
+/*    Please direct questions, bug reports, and contributions to        */
+/*        koethe@informatik.uni-hamburg.de                              */
+/*                                                                      */
+/*  THIS SOFTWARE IS PROVIDED AS IS AND WITHOUT ANY EXPRESS OR          */
+/*  IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED      */
+/*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. */
+/*                                                                      */
+/************************************************************************/
+ 
+
+#include <iostream>
+#include "vigra/stdimage.hxx"
+#include "vigra/distancetransform.hxx"
+#include "vigra/impex.hxx"
+
+using namespace vigra; // MSVC doesn't support Koenig lookup
+
+
+int main(int argc, char ** argv)
+{
+    if(argc != 3)
+    {
+        std::cout << "Usage: " << argv[0] << " infile outfile" << std::endl;
+        std::cout << "(supported formats: " << vigra::impexListFormats() << ")" << std::endl;
+        
+        return 1;
+    }
+    
+    try
+    {
+        // read image given as first argument
+        // file type is determined automatically
+        vigra::ImageImportInfo info(argv[1]);
+        
+        if(info.isGrayscale())
+        {
+            // create a gray scale image of appropriate size
+            vigra::BImage in(info.width(), info.height());
+            vigra::FImage out(info.width(), info.height());
+            
+            // import the image just read
+            importImage(info, destImage(in));
+            
+            distanceTransform(srcImageRange(in),
+                           destImage(out),
+                           255, 2);
+                           
+                           // write the image to the file given as second argument
+            // the file type will be determined from the file name's extension
+            exportImage(srcImageRange(out), vigra::ImageExportInfo(argv[2]));
+        }
+        else
+        {
+            // create a RGB image of appropriate size
+            vigra::BRGBImage in(info.width(), info.height());
+            
+            // import the image just read
+            importImage(info, destImage(in));
+            
+            // write the image to the file given as second argument
+            // the file type will be determined from the file name's extension
+            exportImage(srcImageRange(in), vigra::ImageExportInfo(argv[2]));
+        }
+    }
+    catch (vigra::StdException & e)
+    {
+        // catch any errors that might have occured and print their reason
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+    
+    return 0;
+}
