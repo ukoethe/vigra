bf58af530caeb7fa114d13f391e394240992f2c5
Hans Meine <meine@informatik.uni-hamburg.de>
2003-07-07 13:57:22 +0200

    added preliminary Point2D- and a fair Rect2D-testsuite

--- test/pixeltypes/test.cxx
+++ test/pixeltypes/test.cxx
@@ -317,13 +317,190 @@ struct PixelTypesTestSuite
     }
 };
 
+/********************************************************************/
+
+struct Point2DTest
+{
+    Point2D p11;
+    Point2D p55;
+
+    Point2DTest()
+    : p11(1, 1),
+      p55(5, 5)
+    {}
+
+    void testOperations()
+    {
+        should(-p11 == Point2D(-1, -1));
+        should(p55-p11 == Size2D(4, 4));
+        should(p55-(p55-p11) == p11);
+        should(p55-Diff2D(4, 4) == p11);
+    }
+};
+
+struct Point2DTestSuite
+: public test_suite
+{
+    Point2DTestSuite()
+    : test_suite("Point2DTestSuite")
+    {
+        add(testCase(&Point2DTest::testOperations));
+    }
+};
+
+/********************************************************************/
+
+struct Rect2DTest
+{
+    Rect2D rect1_1;
+    Rect2D emptyRect;
+    Rect2D bigRect;
+
+    Rect2DTest()
+        : rect1_1(Point2D(1, 1), Point2D(2, 2)),
+          bigRect(0, 0, 10, 10)
+    {
+    }
+
+    void testProperties()
+    {
+        shouldEqual(rect1_1.width(), 1);
+        shouldEqual(rect1_1.height(), 1);
+        should(!rect1_1.isEmpty());
+
+        shouldEqual(emptyRect.width(), 0);
+        shouldEqual(emptyRect.height(), 0);
+        should(emptyRect.isEmpty());
+
+        shouldEqual(bigRect.width(), 10);
+        shouldEqual(bigRect.height(), 10);
+        should(!bigRect.isEmpty());
+
+        should(rect1_1 != emptyRect);
+        should(bigRect != emptyRect);
+        should(bigRect != rect1_1);
+
+        bigRect = rect1_1;
+        should(bigRect == rect1_1);
+    }
+
+    void testContains()
+    {
+        should(!emptyRect.contains(Point2D(0, 0)));
+        should(!emptyRect.contains(Point2D(0, 1)));
+        should(!emptyRect.contains(Point2D(0, 2)));
+        should(!emptyRect.contains(Point2D(1, 0)));
+        should(!emptyRect.contains(Point2D(1, 1)));
+        should(!emptyRect.contains(Point2D(1, 2)));
+        should(!emptyRect.contains(Point2D(2, 0)));
+        should(!emptyRect.contains(Point2D(2, 1)));
+        should(!emptyRect.contains(Point2D(2, 2)));
+
+        should( emptyRect.contains(emptyRect));
+        should(!emptyRect.contains(rect1_1));
+        should(!emptyRect.contains(bigRect));
+
+        should(!rect1_1.contains(Point2D(0, 0)));
+        should(!rect1_1.contains(Point2D(0, 1)));
+        should(!rect1_1.contains(Point2D(0, 2)));
+        should(!rect1_1.contains(Point2D(1, 0)));
+        should( rect1_1.contains(Point2D(1, 1)));
+        should(!rect1_1.contains(Point2D(1, 2)));
+        should(!rect1_1.contains(Point2D(2, 0)));
+        should(!rect1_1.contains(Point2D(2, 1)));
+        should(!rect1_1.contains(Point2D(2, 2)));
+
+        should( rect1_1.contains(emptyRect));
+        should( rect1_1.contains(rect1_1));
+        should(!rect1_1.contains(bigRect));
+
+        should(bigRect.contains(Point2D(0, 0)));
+        should(bigRect.contains(Point2D(0, 1)));
+        should(bigRect.contains(Point2D(0, 2)));
+        should(bigRect.contains(Point2D(1, 0)));
+        should(bigRect.contains(Point2D(1, 1)));
+        should(bigRect.contains(Point2D(1, 2)));
+        should(bigRect.contains(Point2D(2, 0)));
+        should(bigRect.contains(Point2D(2, 1)));
+        should(bigRect.contains(Point2D(2, 2)));
+
+        should( bigRect.contains(emptyRect));
+        should( bigRect.contains(rect1_1));
+        should( bigRect.contains(bigRect));
+    }
+
+    void testIntersection()
+    {
+        should(!emptyRect.intersects(emptyRect));
+        should(!emptyRect.intersects(rect1_1));
+        should(!emptyRect.intersects(bigRect));
+        should(!rect1_1.intersects(emptyRect));
+        should( rect1_1.intersects(rect1_1));
+        should( rect1_1.intersects(bigRect));
+        should(!bigRect.intersects(emptyRect));
+        should( bigRect.intersects(rect1_1));
+        should( bigRect.intersects(bigRect));
+
+        should(!bigRect.intersects(Rect2D(Point2D(3, -3), Point2D(3, 3))));
+        should( bigRect.intersects(Rect2D(Point2D(3, -3), Point2D(4, 3))));
+        should( bigRect.intersects(Rect2D(Point2D(3, -3), Point2D(14, 3))));
+
+        should((rect1_1 & emptyRect).isEmpty());
+        should(!(rect1_1 & bigRect).isEmpty());
+        should((rect1_1 & bigRect) == rect1_1);
+    }
+
+    void testUnion()
+    {
+        should(!(rect1_1 | emptyRect).isEmpty());
+        should((rect1_1 | emptyRect) == rect1_1);
+        should((rect1_1 | bigRect) == bigRect);
+        rect1_1 |= Point2D(3, 3);
+        shouldEqual(rect1_1.upperLeft(), Diff2D(1, 1));
+        shouldEqual(rect1_1.lowerRight(), Diff2D(4, 4));
+    }
+
+    void testSizes()
+    {
+        shouldEqual(rect1_1.size(), Size2D(1, 1));
+        shouldEqual(bigRect.size(), Size2D(10, 10));
+        emptyRect.setSize(10, 10);
+        should(bigRect == emptyRect);
+        emptyRect.addSize(Size2D(-4, -7));
+        shouldEqual(emptyRect.size(), Size2D(6, 3));
+        emptyRect.setSize(bigRect.size());
+        should(bigRect == emptyRect);
+    }
+};
+
+struct Rect2DTestSuite
+: public test_suite
+{
+    Rect2DTestSuite()
+    : test_suite("Rect2DTestSuite")
+    {
+        add(testCase(&Rect2DTest::testProperties));
+        add(testCase(&Rect2DTest::testIntersection));
+        add(testCase(&Rect2DTest::testUnion));
+        add(testCase(&Rect2DTest::testSizes));
+    }
+};
+
+/********************************************************************/
+
 int main()
 {
     PixelTypesTestSuite test;
-
     int failed = test.run();
-
     std::cout << test.report() << std::endl;
+
+    Point2DTestSuite test2;
+    failed += test2.run();
+    std::cout << test2.report() << std::endl;
+
+    Rect2DTestSuite test3;
+    failed += test.run();
+    std::cout << test3.report() << std::endl;
 
     return (failed != 0);
 }
