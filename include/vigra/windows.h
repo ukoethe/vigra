690274aa398c6b087cce4b39c374ad98dd0b4f57
Ullrich Koethe <koethe@informatik.uni-hamburg.de>
2003-08-06 22:39:31 +0200

    initial

---
+++ include/vigra/windows.h
@@ -0,0 +1,33 @@
+/************************************************************************/
+/*                                                                      */
+/*               Copyright 2002-2003 by Ullrich Koethe                  */
+/*       Cognitive Systems Group, University of Hamburg, Germany        */
+/*                                                                      */
+/*    This file is part of the VIGRA computer vision library.           */
+/*    You may use, modify, and distribute this software according       */
+/*    to the terms stated in the LICENSE file included in               */
+/*    the VIGRA distribution.                                           */
+/*                                                                      */
+/*    The VIGRA Website is                                              */
+/*        http://kogs-www.informatik.uni-hamburg.de/~koethe/vigra/      */
+/*    Please direct questions, bug reports, and contributions to        */
+/*        koethe@informatik.uni-hamburg.de                              */
+/*                                                                      */
+/*  THIS SOFTWARE IS PROVIDED AS IS AND WITHOUT ANY EXPRESS OR          */
+/*  IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED      */
+/*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. */
+/*                                                                      */
+/************************************************************************/
+ 
+#ifndef VIGRA_WINDOWS_H
+#define VIGRA_WINDOWS_H
+
+// prevent the global namespace to become polluted with 
+// badly named Windows macros
+
+#define NOMINMAX
+#include <windows.h>
+#undef NOMINMAX
+#undef DIFFERENCE
+
+#endif /* VIGRA_WINDOWS_H */
