c624bd4de67e8372f3cbbc202c1cc5141571ff0f
Ullrich Koethe <koethe@informatik.uni-hamburg.de>
2005-02-22 15:40:12 +0100

    added many functions added NumericTraits changed bit allocation policy for add and subtract

--- include/vigra/fixedpoint.hxx
+++ include/vigra/fixedpoint.hxx
@@ -25,7 +25,7 @@
 #include "vigra/mathutil.hxx"
 #include "vigra/static_assert.hxx"
 #include "vigra/error.hxx"
-#include <functional>
+#include "vigra/numerictraits.hxx"
 
 namespace vigra {
 
@@ -44,14 +44,20 @@ public:
 //    typedef Error_FixedPointTraits_not_specialized_for_this_case DividesType;
 };
 
+// return type policy: 
+//     * try to allocate enough bits to represent the biggest possible result
+//     * in case of ass/subtract: if all bits of the internal int are used up, 
+//                                keep the representation
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
 class FixedPointTraits<FixedPoint<IntBits1, FracBits1>, FixedPoint<IntBits2, FracBits2> >
 {
     enum { MaxIntBits  = (IntBits1 < IntBits2) ? IntBits2 : IntBits1,
-           MaxFracBits = (FracBits1 < FracBits2) ? FracBits2 : FracBits1 };
+           MaxFracBits = (FracBits1 < FracBits2) ? FracBits2 : FracBits1,
+           PlusMinusIntBits = (MaxIntBits + 1 + MaxFracBits < 32) ?
+                               MaxIntBits + 1 : MaxIntBits};
 public:
-    typedef FixedPoint<MaxIntBits + 1, MaxFracBits>                 PlusType;
-    typedef FixedPoint<MaxIntBits + 1, MaxFracBits>                 MinusType;
+    typedef FixedPoint<PlusMinusIntBits, MaxFracBits>               PlusType;
+    typedef FixedPoint<PlusMinusIntBits, MaxFracBits>               MinusType;
     typedef FixedPoint<IntBits1 + IntBits2, FracBits1 + FracBits2>  MultipliesType;
 //    typedef FixedPoint<IntBits1 + FracBits2, FracBits1 + IntBits2>  DividesType;
 };
@@ -60,6 +66,7 @@ template <int N>
 struct FixedPoint_overflow_error__More_than_31_bits_requested
 : staticAssert::AssertBool<(N < 32)>
 {};
+
 
 template <bool Predicate>
 struct FixedPoint_assignment_error__Target_object_has_too_few_integer_bits
@@ -84,9 +91,9 @@ template <>
 struct FPAssignWithRound<true>
 {
     template <int N>
-    static inline int exec(int const v) 
-    { 
-        return (v + (1 << (N - 1))) >> (N); 
+    static inline int exec(int const v)
+    {
+        return (v + (1 << (N - 1))) >> (N);
     }
 };
 
@@ -104,15 +111,15 @@ template <>
 struct FPMulImplementation<true>
 {
     template <int N>
-    static inline int exec(int l, int r) 
-    { 
-        // there is not enough space in the result 
+    static inline int exec(int l, int r)
+    {
+        // there is not enough space in the result
         // => perform calculations that preserve as much accuracy as possible
         enum { diffl = N / 2, diffr = N  - diffl, maskl = (1 << diffl) - 1, maskr = (1 << diffr) - 1 };
         int shiftl = l >> diffl;
         int shiftr = r >> diffr;
 
-        return shiftl * shiftr + (((l & maskl) * shiftr) >> diffl) + 
+        return shiftl * shiftr + (((l & maskl) * shiftr) >> diffl) +
                                  (((r & maskr) * shiftl) >> diffr);
     }
 };
@@ -127,25 +134,27 @@ public:
         INT_BITS        = IntBits,
         FRACTIONAL_BITS = FractionalBits,
         TOTAL_BITS      = IntBits + FractionalBits,
+        MAX             = (int)(((unsigned)1 << TOTAL_BITS) - 1),
         ONE             = 1 << FractionalBits,
         ONE_HALF        = ONE >> 1,
         FRACTIONAL_MASK = ONE - 1,
-        INT_MASK        = !FRACTIONAL_MASK
+        INT_MASK        = MAX ^ FRACTIONAL_MASK
     };
 
     int value;
 
-        /** 
+    FixedPoint()
+    {}
+
+        /**
         */
-    FixedPoint(int v = 0)
+    explicit FixedPoint(int v)
     : value(v << FractionalBits)
     {
         VIGRA_STATIC_ASSERT((FixedPoint_overflow_error__More_than_31_bits_requested<(IntBits + FractionalBits)>));
-        vigra_precondition(abs(v) < (1 << IntBits),
-            "FixedPoint(int v): Too few integer bits to represent v.");
     }
 
-        /** 
+        /**
         */
     FixedPoint(int v, FixedPointNoShift)
     : value(v)
@@ -153,10 +162,10 @@ public:
         VIGRA_STATIC_ASSERT((FixedPoint_overflow_error__More_than_31_bits_requested<(IntBits + FractionalBits)>));
     }
 
-    FixedPoint(double rhs)
+    explicit FixedPoint(double rhs)
     : value((int)round(rhs * ONE))
     {
-        vigra_precondition(abs(round(rhs * ONE)) < (1 << TOTAL_BITS),
+        vigra_precondition(abs(rhs * ONE) <= (double)MAX,
             "FixedPoint(double rhs): Too few integer bits to convert rhs.");
     }
 
@@ -202,10 +211,64 @@ public:
         return *this;
     }
 
+    operator double() const
+    {
+        return (double)value / ONE;
+    }
 
     FixedPoint operator-() const
     {
         return FixedPoint(-value, FPNoShift);
+    }
+
+    FixedPoint & operator++()
+    {
+        value += ONE;
+        return *this;
+    }
+
+    FixedPoint operator++(int)
+    {
+        FixedPoint old(*this);
+        value += ONE;
+        return old;
+    }
+
+    FixedPoint & operator--()
+    {
+        value -= ONE;
+        return *this;
+    }
+
+    FixedPoint operator--(int)
+    {
+        FixedPoint old(*this);
+        value -= ONE;
+        return old;
+    }
+
+    template <unsigned Int2, unsigned Frac2>
+    FixedPoint & operator+=(const FixedPoint<Int2, Frac2> &other)
+    {
+        VIGRA_STATIC_ASSERT((FixedPoint_assignment_error__Target_object_has_too_few_integer_bits<(IntBits >= Int2)>));
+        value += detail::FPAssignWithRound<(Frac2 > FractionalBits)>::template exec<Frac2 - FractionalBits>(other.value);
+        return *this;
+    }
+
+    template <unsigned Int2, unsigned Frac2>
+    FixedPoint & operator-=(const FixedPoint<Int2, Frac2> &other)
+    {
+        VIGRA_STATIC_ASSERT((FixedPoint_assignment_error__Target_object_has_too_few_integer_bits<(IntBits >= Int2)>));
+        value -= detail::FPAssignWithRound<(Frac2 > FractionalBits)>::template exec<Frac2 - FractionalBits>(other.value);
+        return *this;
+    }
+    
+    template <unsigned Int2, unsigned Frac2>
+    FixedPoint & operator*=(const FixedPoint<Int2, Frac2> &other)
+    {
+        VIGRA_STATIC_ASSERT((FixedPoint_assignment_error__Target_object_has_too_few_integer_bits<(IntBits >= Int2)>));
+        value = detail::FPMulImplementation<(Frac2 > 0)>::template exec<Frac2>(value, other.value);
+        return *this;
     }
 };
 
@@ -224,7 +287,7 @@ VIGRA_FIXED_POINT_FACTORY(int, 31)
 #undef VIGRA_FIXED_POINT_FACTORY
 
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
-inline 
+inline
 bool operator==(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 {
     enum { MaxFracBits = (FracBits1 < FracBits2) ? FracBits2 : FracBits1 };
@@ -232,7 +295,7 @@ bool operator==(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits
 }
 
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
-inline 
+inline
 bool operator!=(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 {
     enum { MaxFracBits = (FracBits1 < FracBits2) ? FracBits2 : FracBits1 };
@@ -240,7 +303,7 @@ bool operator!=(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits
 }
 
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
-inline 
+inline
 bool operator<(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 {
     enum { MaxFracBits = (FracBits1 < FracBits2) ? FracBits2 : FracBits1 };
@@ -248,7 +311,7 @@ bool operator<(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2
 }
 
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
-inline 
+inline
 bool operator<=(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 {
     enum { MaxFracBits = (FracBits1 < FracBits2) ? FracBits2 : FracBits1 };
@@ -256,7 +319,7 @@ bool operator<=(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits
 }
 
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
-inline 
+inline
 bool operator>(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 {
     enum { MaxFracBits = (FracBits1 < FracBits2) ? FracBits2 : FracBits1 };
@@ -264,7 +327,7 @@ bool operator>(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2
 }
 
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
-inline 
+inline
 bool operator>=(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 {
     enum { MaxFracBits = (FracBits1 < FracBits2) ? FracBits2 : FracBits1 };
@@ -272,12 +335,12 @@ bool operator>=(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits
 }
 
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
-inline 
+inline
 typename FixedPointTraits<FixedPoint<IntBits1, FracBits1>, FixedPoint<IntBits2, FracBits2> >::PlusType
 operator+(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 {
     enum { MaxFracBits = (FracBits1 < FracBits2) ? FracBits2 : FracBits1 };
-    return typename 
+    return typename
         FixedPointTraits<FixedPoint<IntBits1, FracBits1>, FixedPoint<IntBits2, FracBits2> >::
         PlusType((l.value << (MaxFracBits - FracBits1)) + (r.value << (MaxFracBits - FracBits2)), FPNoShift);
 }
@@ -285,19 +348,19 @@ operator+(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2,
           unsigned IntBits3, unsigned FracBits3>
 inline void
-add(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r, 
+add(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r,
     FixedPoint<IntBits3, FracBits3> & result)
 {
     result = l + r;
 }
 
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
-inline 
+inline
 typename FixedPointTraits<FixedPoint<IntBits1, FracBits1>, FixedPoint<IntBits2, FracBits2> >::MinusType
 operator-(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 {
     enum { MaxFracBits = (FracBits1 < FracBits2) ? FracBits2 : FracBits1 };
-    return typename 
+    return typename
         FixedPointTraits<FixedPoint<IntBits1, FracBits1>, FixedPoint<IntBits2, FracBits2> >::
         MinusType((l.value << (MaxFracBits - FracBits1)) - (r.value << (MaxFracBits - FracBits2)), FPNoShift);
 }
@@ -305,18 +368,18 @@ operator-(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2,
           unsigned IntBits3, unsigned FracBits3>
 inline void
-sub(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r, 
+sub(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r,
     FixedPoint<IntBits3, FracBits3> & result)
 {
     result = l - r;
 }
 
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
-inline 
+inline
 typename FixedPointTraits<FixedPoint<IntBits1, FracBits1>, FixedPoint<IntBits2, FracBits2> >::MultipliesType
 operator*(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 {
-    return typename 
+    return typename
         FixedPointTraits<FixedPoint<IntBits1, FracBits1>, FixedPoint<IntBits2, FracBits2> >::
         MultipliesType(l.value * r.value, FPNoShift);
 }
@@ -324,12 +387,12 @@ operator*(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r)
 template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2,
           unsigned IntBits3, unsigned FracBits3>
 inline void
-mul(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r, 
+mul(FixedPoint<IntBits1, FracBits1> l, FixedPoint<IntBits2, FracBits2> r,
     FixedPoint<IntBits3, FracBits3> & result)
 {
     VIGRA_STATIC_ASSERT((FixedPoint_assignment_error__Target_object_has_too_few_integer_bits<(IntBits1 + IntBits2 <= IntBits3)>));
-    enum { diff = FracBits1 + FracBits1 - FracBits3 };
-    result.value = detail::FPMulImplementation<(diff > 0)>::template exec<diff>(l.value, r.value); 
+    enum { diff = FracBits1 + FracBits2 - FracBits3 };
+    result.value = detail::FPMulImplementation<(diff > 0)>::template exec<diff>(l.value, r.value);
 }
 
 template <unsigned IntBits, unsigned FracBits>
@@ -337,6 +400,21 @@ inline FixedPoint<IntBits, FracBits>
 abs(FixedPoint<IntBits, FracBits> v)
 {
     return FixedPoint<IntBits, FracBits>(abs(v.value), FPNoShift);
+}
+
+template <unsigned IntBits, unsigned FracBits>
+inline FixedPoint<0, FracBits>
+frac(FixedPoint<IntBits, FracBits> v)
+{
+    return FixedPoint<0, FracBits>(v.value & FixedPoint<IntBits, FracBits>::FRACTIONAL_MASK, FPNoShift);
+}
+
+template <unsigned IntBits, unsigned FracBits>
+inline FixedPoint<0, FracBits>
+dual_frac(FixedPoint<IntBits, FracBits> v)
+{
+    return FixedPoint<0, FracBits>(FixedPoint<0, FracBits>::ONE - 
+                                   (v.value & FixedPoint<IntBits, FracBits>::FRACTIONAL_MASK), FPNoShift);
 }
 
 template <unsigned IntBits, unsigned FracBits>
@@ -359,6 +437,38 @@ round(FixedPoint<IntBits, FracBits> v)
 {
     return((v.value + FixedPoint<IntBits, FracBits>::ONE_HALF) >> FracBits);
 }
+
+template <unsigned IntBits, unsigned FracBits>
+struct NumericTraits<FixedPoint<IntBits, FracBits> >
+{
+    typedef FixedPoint<IntBits, FracBits> Type;
+        //typedef FixedPoint<IntBits, FracBits> Promote;
+        //typedef FixedPoint<IntBits, FracBits> RealPromote;
+        //typedef std::complex<RealPromote> ComplexPromote;
+    typedef Type ValueType;
+
+        //typedef VigraFalseType isIntegral;
+    typedef VigraTrueType  isScalar;
+    typedef VigraTrueType  isOrdered;
+    typedef VigraFalseType isComplex;
+
+    static Type zero() { return Type(0, FPNoShift); }
+    static Type one() { return Type(Type::ONE, FPNoShift); }
+    static Type nonZero() { return one(); }
+    static Type epsilon() { return Type(1, FPNoShift); }
+    static Type smallestPositive() { return Type(1, FPNoShift); }
+    static Type max() { return Type( Type::MAX, FPNoShift); }
+    static Type min() { return -max(); }
+};
+
+template <unsigned IntBits1, unsigned FracBits1, unsigned IntBits2, unsigned FracBits2>
+struct PromoteTraits<FixedPoint<IntBits1, FracBits1>,
+                     FixedPoint<IntBits2, FracBits2> >
+{
+    typedef typename 
+        FixedPointTraits<FixedPoint<IntBits1, FracBits1>, FixedPoint<IntBits2, FracBits2> >::PlusType 
+        Promote;
+};
 
 } // namespace vigra
 
