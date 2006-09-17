49269e759fbe07229c9c442152fa6b2c4b7b92b7
Hans Meine <meine@informatik.uni-hamburg.de>
2006-09-17 13:42:20 +0200

    reformatting, correct comment

--- include/vigra/multi_impex.hxx
+++ include/vigra/multi_impex.hxx
@@ -30,7 +30,7 @@
 /*    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,      */
 /*    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      */
 /*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR     */
-/*    OTHER DEALINGS IN THE SOFTWARE.                                   */                
+/*    OTHER DEALINGS IN THE SOFTWARE.                                   */
 /*                                                                      */
 /************************************************************************/
 
@@ -53,7 +53,7 @@ namespace vigra {
 VIGRA_EXPORT void findImageSequence(const std::string &name_base,
                        const std::string &name_ext,
                        std::vector<std::string> & numbers);
-                       
+
 /** \addtogroup VolumeImpex Import/export of volume data.
 */
 
@@ -69,12 +69,12 @@ VIGRA_EXPORT void findImageSequence(const std::string &name_base,
 
     The data are expected to be stored in a by-slice manner,
     where the slices are enumerated from <tt>name_base+"[0-9]+"+name_ext</tt>.
-    <tt>name_base</tt> may contain a path. All slice files with the same name base and 
+    <tt>name_base</tt> may contain a path. All slice files with the same name base and
     extension are considered part of the same volume. Slice numbers must be non-negative,
-    but can otherwise start anywhere and need not be successive. Slices will be read 
-    in ascending numerical (not lexicographic) order. All slices must have the 
-    same size. The <tt>volume</tt> will be reshaped to match the count and 
-    size of the slices found. 
+    but can otherwise start anywhere and need not be successive. Slices will be read
+    in ascending numerical (not lexicographic) order. All slices must have the
+    same size. The <tt>volume</tt> will be reshaped to match the count and
+    size of the slices found.
 
     <b>\#include</b>
     "<a href="multi__impex_8hxx-source.html">vigra/multi_impex.hxx</a>"
@@ -88,19 +88,19 @@ void importVolume (MultiArray <3, T, Allocator> & volume,
 {
     std::vector<std::string> numbers;
     findImageSequence(name_base, name_ext, numbers);
-    
+
     std::string message("importVolume(): No files matching '");
     message += name_base + "[0-9]+" + name_ext + "' found.";
     vigra_precondition(numbers.size() > 0, message.c_str());
-          
-    for (unsigned int i = 0; i < numbers.size(); ++i) 
+
+    for (unsigned int i = 0; i < numbers.size(); ++i)
     {
         // build the filename
         std::string name = name_base + numbers[i] + name_ext;
 
         // import the image
         ImageImportInfo info (name.c_str ());
-        
+
         // reshape the array according to size of first image
         if(i == 0)
         {
@@ -113,7 +113,7 @@ void importVolume (MultiArray <3, T, Allocator> & volume,
         BasicImageView <T> view = makeBasicImageView (array_view);
         vigra_precondition(view.size() == info.size(),
             "importVolume(): image size mismatch.");
-            
+
         importImage (info, destImage(view));
     }
 }
@@ -128,7 +128,7 @@ void importVolume (MultiArray <3, T, Allocator> & volume,
 /** \brief Function for exporting a 3D volume.
 
     The volume is exported in a by-slice manner, where the number of slices equals
-    the depth of the volume. The file names will be enumerated like 
+    the depth of the volume. The file names will be enumerated like
     <tt>name_base+"000"+name_ext</tt>, <tt>name_base+"001"+name_ext</tt> etc.
     (the actual number of zeros depends on the depth).
 
@@ -142,10 +142,10 @@ void exportVolume (MultiArrayView <3, T, Tag> const & volume,
                    const std::string &name_base,
                    const std::string &name_ext)
 {
-    
+
     const unsigned int depth = volume.shape (2);
     int numlen = static_cast <int> (std::ceil (std::log10 ((double)depth)));
-    for (unsigned int i = 0; i < depth; ++i) 
+    for (unsigned int i = 0; i < depth; ++i)
     {
 
         // build the filename
@@ -159,9 +159,9 @@ void exportVolume (MultiArrayView <3, T, Tag> const & volume,
         MultiArrayView <2, T, Tag> array_view (volume.bindOuter (i));
         BasicImageView <T> view = makeBasicImageView (array_view);
 
-        // import the image
-        ImageExportInfo info (name.c_str ());
-        exportImage (srcImageRange(view), info);
+        // export the image
+        ImageExportInfo info(name.c_str ());
+        exportImage(srcImageRange(view), info);
     }
 }
 
