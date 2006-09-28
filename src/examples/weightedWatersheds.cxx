6af0580428c04c7aabf7f1da9488087070094f31
Ullrich Koethe <koethe@informatik.uni-hamburg.de>
2006-09-28 21:30:57 +0200

    moved work-around for gcc 2.95.3 to functorexpression.hxx

--- src/examples/weightedWatersheds.cxx
+++ src/examples/weightedWatersheds.cxx
@@ -48,10 +48,6 @@
 
 using namespace vigra; 
 
-#if defined(__GNUC__) && __GNUC__ < 3
-using namespace vigra::functor; 
-#endif
-
 // define a functor that calsulates the squared magnitude of the gradient
 // given the x- and y- components of the gradient
 struct GradientSquaredMagnitudeFunctor
