df336902c448c927f0e35ad776db506e82aa6f83
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2008-08-20 14:13:27 +0200

    documentation wrapper
    --HG--
    branch : matlab-extract

---
+++ src/matlab/resizeVolume.m
@@ -0,0 +1,9 @@
+function resized = resizeVolume(original, newShape)
+% function resized = resizeVolume(original, newShape)
+%
+% resize a 3D array to a new shape using cobic spline interpolation.
+%
+%     original - 3D matrix with original data
+%     newShape - int32-array of length 3 giving the new shape
+%
+    error('mex-file for resizeVolume() missing. Call buildVigraExtensions() to create it.')
