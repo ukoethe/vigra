286a5add7fcf7ee372118eda642601a3f5061c07
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2008-08-25 19:12:43 +0200

    extended to support different spline orders
    --HG--
    branch : matlab-extract

--- src/matlab/resizeImage.cpp
+++ src/matlab/resizeImage.cpp
@@ -11,23 +11,52 @@ void vigraResizeImageImpl(matlab::OutputArray outputs, matlab::InputArray inputs
     MultiArrayShape<2>::type newShape = matlab::getShape<2>(inputs[1]);
     MultiArrayShape<3>::type newShape3(newShape[0], newShape[1], in.shape(2));
     MultiArrayView<3, T> out = matlab::createMultiArray<3, T>(newShape3, outputs[0]);
+    unsigned int splineOrder = inputs.isValid(2) 
+                                   ? matlab::getScalar<unsigned int>(inputs[2])
+                                   : 3;
 
     for(int k=0; k<in.shape(2); ++k)
     {
         BasicImageView<T> ink = makeBasicImageView(in.bindOuter(k));    
         BasicImageView<T> outk = makeBasicImageView(out.bindOuter(k));    
-        resizeImageSplineInterpolation(srcImageRange(ink), destImageRange(outk));
+        switch(splineOrder)
+        {
+          case 0:
+            resizeImageNoInterpolation(srcImageRange(ink), destImageRange(outk));
+            break;
+          case 1:
+            resizeImageLinearInterpolation(srcImageRange(ink), destImageRange(outk));
+            break;
+          case 2:
+            resizeImageSplineInterpolation(srcImageRange(ink), destImageRange(outk), BSpline<2>());
+            break;
+          case 3:
+            resizeImageSplineInterpolation(srcImageRange(ink), destImageRange(outk), BSpline<3>());
+            break;
+          case 4:
+            resizeImageSplineInterpolation(srcImageRange(ink), destImageRange(outk), BSpline<4>());
+            break;
+          case 5:
+            resizeImageSplineInterpolation(srcImageRange(ink), destImageRange(outk), BSpline<5>());
+            break;
+          default:
+            mexErrMsgTxt("Interpolation order must be between 0 and 5.");
+        }
     }
 }
 
  
 /** MATLAB 
 function resized = resizeImage(original, newShape)
+function resized = resizeImage(original, newShape, splineOrder)
 
-resize a 2D array to a new shape using cubic spline interpolation.
+resize a 2D array to a new shape using spline interpolation of the given order 
+(default: using cubic spline interpolation).
     original - 2D matrix with original image 
                (gray scale or multi-band/RGB, type 'uint8', 'single' or 'double')
     newShape - int32-array of length 2 giving the new shape
+    splineOrder - order of interpolation 
+                  (0 <= splineOrder <= 5, default: 3, i.e. cubic splines)
 */
 void vigraMexFunction(matlab::OutputArray outputs, matlab::InputArray inputs)
 {    
--- src/matlab/resizeVolume.cpp
+++ src/matlab/resizeVolume.cpp
@@ -3,18 +3,68 @@
 
 using namespace vigra;
 
+template <class T>
+void vigraResizeVolumeImpl(matlab::OutputArray outputs, matlab::InputArray inputs)
+{    
+    MultiArrayView<4, T> in = matlab::getMultiArray<4, T>(inputs[0]);
+    MultiArrayShape<3>::type newShape = matlab::getShape<3>(inputs[1]);
+    MultiArrayShape<4>::type newShape4(newShape[0], newShape[1], newShape[2], in.shape(3));
+    MultiArrayView<4, T> out = matlab::createMultiArray<4, T>(newShape4, outputs[0]);
+    unsigned int splineOrder = inputs.isValid(2) 
+                                   ? matlab::getScalar<unsigned int>(inputs[2])
+                                   : 3;
+
+    for(int k=0; k<in.shape(3); ++k)
+    {
+        MultiArrayView<3, T> ink = in.bindOuter(k);    
+        MultiArrayView<3, T> outk = out.bindOuter(k);
+        switch(splineOrder)
+        {
+          case 0:
+            resizeMultiArraySplineInterpolation(srcMultiArrayRange(ink), destMultiArrayRange(outk), BSpline<0>());
+            break;
+          case 1:
+            resizeMultiArraySplineInterpolation(srcMultiArrayRange(ink), destMultiArrayRange(outk), BSpline<1>());
+            break;
+          case 2:
+            resizeMultiArraySplineInterpolation(srcMultiArrayRange(ink), destMultiArrayRange(outk), BSpline<2>());
+            break;
+          case 3:
+            resizeMultiArraySplineInterpolation(srcMultiArrayRange(ink), destMultiArrayRange(outk), BSpline<3>());
+            break;
+          case 4:
+            resizeMultiArraySplineInterpolation(srcMultiArrayRange(ink), destMultiArrayRange(outk), BSpline<4>());
+            break;
+          case 5:
+            resizeMultiArraySplineInterpolation(srcMultiArrayRange(ink), destMultiArrayRange(outk), BSpline<5>());
+            break;
+          default:
+            mexErrMsgTxt("Interpolation order must be between 0 and 5.");
+        }
+    }
+}
+
+ 
 /** MATLAB 
 function resized = resizeVolume(original, newShape)
+function resized = resizeVolume(original, newShape, splineOrder)
 
-resize a 3D array to a new shape using cubic spline interpolation.
-    original - 3D matrix with original data
-    newShape - int32-array of length 3 giving the new shape
+resize a 3D array to a new shape using spline interpolation of the given order 
+(default: using cubic spline interpolation).
+    original    - 3D matrix with original volume data 
+                  (gray scale or multi-band/RGB, type 'uint8', 'single' or 'double')
+    newShape    - int32-array of length 3 giving the new shape
+    splineOrder - order of interpolation 
+                  (0 <= splineOrder <= 5, default: 3, i.e. cubic splines)
 */
 void vigraMexFunction(matlab::OutputArray outputs, matlab::InputArray inputs)
-{
-    MultiArrayView<3, double> in = matlab::getMultiArray<3, double>(inputs[0]);    
-    MultiArrayShape<3>::type newShape = matlab::getShape<3>(inputs[1]);
-    MultiArrayView<3, double> out = matlab::createMultiArray<3, double>(newShape, outputs[0]);
-
-    resizeMultiArraySplineInterpolation(srcMultiArrayRange(in), destMultiArrayRange(out));
+{    
+    if(matlab::ValueType<UInt8>::check(inputs[0]))
+        vigraResizeVolumeImpl<UInt8>(outputs, inputs);
+    else if(matlab::ValueType<float>::check(inputs[0]))
+        vigraResizeVolumeImpl<float>(outputs, inputs);
+    else if(matlab::ValueType<double>::check(inputs[0]))
+        vigraResizeVolumeImpl<double>(outputs, inputs);
+    else
+        mexErrMsgTxt("Input image must have type 'uint8', 'single' or 'double'.");
 }
