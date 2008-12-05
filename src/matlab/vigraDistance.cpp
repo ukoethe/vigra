6c5d1da0e35c723208e5a986fe8917651e51190f
Rahul Nair <rahul.nair@iwr.uni-heidelberg.de>
2008-12-05 16:18:50 +0100

    vigraDistance now supports a signed backgroundMode
    --HG--
    extra : transplant_source : %F4%A2%A6%88%2B%7C%85%19%D3%A3%E7%DA%EC%A6U%95%9B%1B%05%3E

--- src/matlab/vigraDistance.cpp
+++ src/matlab/vigraDistance.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <vigra/distancetransform.hxx>
 #include <vigra/multi_distance.hxx>
-
+#include <functional> 
 
 //this could be a typedef but if you want outType to be the same type as inType then you can just 
 //set outType to T
@@ -18,7 +18,7 @@ using namespace vigra;
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 template <class T>
 struct data: public base_data<T>{
-	declScalarMinMax(bool, backgroundMode, 0, 0, 1);
+	declScalarMinMax(int, backgroundMode, 0, 0, 2);
 	declCharConstr(method, 4, MULT, MULT_SQUARED, IMAG_DIST_TRANS, INVERTEDCRAP, e);
 	declOut(double);
 	
@@ -28,9 +28,11 @@ struct data: public base_data<T>{
 	declScalar(T, backgroundPixel, 0);
 	declScalarMinMax(int, norm, 2, 0, 2);
 	
+	MultiArray<3, double> tmp3D;
+	
 	data(matlab::OutputArray outputs, matlab::InputArray inputs)
 	:			base_data(inputs), map(backgroundMode), map(method), map(backgroundPixel), map(norm),
-				pitch3D(1.0,1.0,1.0)
+				pitch3D(1.0,1.0,1.0), tmp3D(in3D.shape())
 	{
 		mapOut_SAME(double);
 		if(inputs.size() == 2)
@@ -44,25 +46,40 @@ struct data: public base_data<T>{
 					pitch3D[1] = temp[1]>0? temp[1]:1.0;
 				}else{
 					TinyVectorView<double, 3> temp = matlab::getVector<double,3>(pitchArr);
-					pitch3D = temp;
+					pitch3D[0] = temp[0]>0? temp[0]:1.0;
+					pitch3D[1] = temp[1]>0? temp[1]:1.0;
+					pitch3D[2] = temp[2]>0? temp[2]:1.0;
 				}
 				if(this->method != MULT){
 					mexWarnMsgTxt("pitch option is only supported with the method MULT");
 				}
 			}
 		}
-		else
-		if(this->numOfDim == VOLUME && this->method == IMAG_DIST_TRANS){
-			this->method = MULT;
-			mexWarnMsgTxt("IMAG_DIST_TRANS only works with 2D Images using default: MULT");
+		if(this->method == IMAG_DIST_TRANS){
+			backgroundMode = 0;
+			if(this->numOfDim == VOLUME){
+				this->method = MULT;
+				mexWarnMsgTxt("IMAG_DIST_TRANS only works with 2D Images using default: MULT");
+			}
 		}
+
 	}
 };
+
+    struct signChangeFunctor
+    {
+        template <class PixelType>
+        PixelType operator()(PixelType const& v) const
+        {
+            return 0.0-v;
+        }
+    };
+
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 /* This function does all the work
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-#define cP2_(a, b) cP<data<T>::a, data<T>::b>::value
+#define cP2_(a, b) cP<a, data<T>::b>::value
 struct vigraFunctor
 {
 	template <class T>
@@ -71,21 +88,35 @@ struct vigraFunctor
 		data<T>  o(outputs, inputs);
 
 		// contorPair maps 2 integers bijectively onto one dimension. (see Wikipedia Cantor pair Function) 
-		switch(cantorPair(o.numOfDim, o.method)){
+		switch(cantorPair(o.backgroundMode == 2, o.method)){
 			//In this case function pointers may have been more elegant.
-			case cP2_(IMAG, MULT):
-				separableMultiDistance(srcMultiArrayRange(o.in3D), destMultiArray(o.out3D), o.backgroundMode, o.pitch3D);
+			case cP2_(0, MULT):
+					separableMultiDistance(srcMultiArrayRange(o.in3D), destMultiArray(o.out3D), o.backgroundMode == 1, o.pitch3D);
+					break;
+			case cP2_(1, MULT):
+				separableMultiDistance(srcMultiArrayRange(o.in3D), destMultiArray(o.out3D), 0, o.pitch3D);
+				transformMultiArray(srcMultiArrayRange(o.out3D), destMultiArray(o.out3D), signChangeFunctor());
+				separableMultiDistance(srcMultiArrayRange(o.in3D), destMultiArray(o.tmp3D), 1, o.pitch3D);
+				combineTwoMultiArrays(
+							srcMultiArrayRange(o.tmp3D), 
+							srcMultiArray(o.out3D), 
+							destMultiArray(o.out3D),  
+							std::plus<double>());
 				break;
-			case cP2_(VOLUME, MULT):
-				separableMultiDistance(srcMultiArrayRange(o.in3D), destMultiArray(o.out3D), o.backgroundMode, o.pitch3D);
+			case cP2_(0, MULT_SQUARED):
+				separableMultiDistSquared(srcMultiArrayRange(o.in3D), destMultiArray(o.out3D), o.backgroundMode == 1);
 				break;
-			case cP2_(IMAG, MULT_SQUARED):
-				separableMultiDistSquared(srcMultiArrayRange(o.in3D), destMultiArray(o.out3D), o.backgroundMode);
+			case cP2_(1, MULT_SQUARED):
+				separableMultiDistSquared(srcMultiArrayRange(o.in3D), destMultiArray(o.out3D), 0);
+				transformMultiArray(srcMultiArrayRange(o.out3D), destMultiArray(o.out3D), signChangeFunctor());
+				separableMultiDistSquared(srcMultiArrayRange(o.in3D), destMultiArray(o.tmp3D), 1);
+				combineTwoMultiArrays(
+							srcMultiArrayRange(o.tmp3D), 
+							srcMultiArray(o.out3D), 
+							destMultiArray(o.out3D),  
+							std::plus<double>());
 				break;
-			case cP2_(VOLUME, MULT_SQUARED):
-				separableMultiDistSquared(srcMultiArrayRange(o.in3D), destMultiArray(o.out3D), o.backgroundMode);
-				break;
-			case cP2_(IMAG, IMAG_DIST_TRANS):
+			case cP2_(0, IMAG_DIST_TRANS):
 				distanceTransform(srcImageRange(o.in), destImage(o.out),o.backgroundPixel, o.norm);
 				break;
 			default:
@@ -115,10 +146,12 @@ options is a struct with possible fields: "method", "backgroundMode" and "backgr
 						(vigra::seperableMultiDist....).MULT_SQUARED returns the squared values of MULT. 
 						IMAG_DIST_TRANS is defined with BasicImage and is slower. Use it only if  you explicitely need the backgroundPixel
 						or norm option. (vigra::distanceTransform)
-"backgroundMode": 		0 (default) ,  1: 	
+"backgroundMode": 		0 (default) ,  1, 2: 	
 						This option is only used with methods MULT and MULT_SQUARED
 						If the parameter background is 1, then the squared distance of all background pixels to the nearest object is calculated. 
 						Otherwise, the distance of all object pixels to the nearest background pixel is calculated.
+						If the parameter background is 2, then the distance if background pixels to the nearest object will be negative valued and
+						the distance of object pixels to the background pixels shall be positive
 "backgroundPixel: 		0(default) , arb. value in grayscale range: 		
 						This option defines the background Pixel value. Only used with method = IMAG_DIST_TRANS
 "norm": 					2(default), 1 , 0.  
