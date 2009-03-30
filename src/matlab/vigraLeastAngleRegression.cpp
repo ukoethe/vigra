ed899990b90f4a4ee6c22fff65c28fbcec965d2e
Rahul.Nar <none@none>
2009-03-30 17:07:58 +0200

    added Docstring - (Not sure whether correct)
    --HG--
    extra : transplant_source : %CC0%98%AC%2C%A8%0Bz%E6%BC%ACS%3D%F8%AD%0A%C73y%9A

--- src/matlab/vigraLeastAngleRegression.cpp
+++ src/matlab/vigraLeastAngleRegression.cpp
@@ -96,21 +96,18 @@ void vigraMexFunction(vigra::matlab::OutputArray outputs, vigra::matlab::InputAr
     }
 }
 /** MATLAB
-function D = vigraRadialSymmetry(inputImage)
-function D = vigraradialSymmetry(inputImage, options);
+function dense_lsq = vigraLeastAngleRegression(A, b)
+function dense_lsq = vigraLeastAngleRegression(A, b, options);
+function [dense_lsq dense_lasso] = vigraLeastAngleRegression(...);
 
-D = vigraRadialSymmetry(inputImage) computes the Fast Radial Symmetry Transform
-            using default options, see vigra::RadialSymmetryTransform for more information.
-D = vigraRadialSymmetry(inputImage, options)  does the same with user options.
+Solves Equations of Type A*x = b using L1 regularisation
+The columns of dense_lsq are the least squares Solutions in the
+    (iterations?) of the Lasso
+Columns of dense_lasso are...
 
-inputImage - 2D input array
 options    - a struct with following possible fields:
-    'scale':    1.0 (default), any positive floating point value
-                scale parameter for the vigraRadialSymmetry
+    'mode':    default 'lasso', nnlasso (non negative lasso), lars (least angle regression)
+    'max_solution_count':   default: Unused, Integral value > 0
 
-
-Usage:
-    opt = struct('method' ,value);
-    out = vigraRadialSymmetry(in, opt);
 
 */
