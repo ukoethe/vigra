9336b30ace222775d4691e88696759c6e5dfaae2
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2009-10-16 20:49:12 +0200

    explicitly set ...DIR variables to NOTFOUND after an unsuccessful FIND_PACKAGE

--- config/VigraFindPackage.cmake
+++ config/VigraFindPackage.cmake
@@ -1,4 +1,10 @@
-# - 
+# - improved version of FIND_PACKAGE
+# FIND_PACKAGE is dangerous when custom search prefixes are added, because these prefixes are tried for 
+# includes and libraries independently. Thus, it may happen that a library is found under one prefix,
+# whereas the corresponding include is found under another. Crashes of the compiled programs are
+# then very likely. VIGRA_FIND_PACKAGE improves upon this by trying custom prefixes one at a time,
+# so that either both libraries are found under the same prefix, or the search is marked as unsuccessful
+# for this prefix.
 #
 MACRO(VIGRA_FIND_PACKAGE package)
     foreach(name ${ARGN})
@@ -11,6 +17,9 @@ MACRO(VIGRA_FIND_PACKAGE package)
         if(NOT ${${package}_FOUND} MATCHES TRUE)
             SET(CMAKE_INCLUDE_PATH ${path}/include)
             SET(CMAKE_LIBRARY_PATH ${path}/lib)
+            SET(${package}_INCLUDE_DIR ${package}_INCLUDE_DIR-NOTFOUND)
+            SET(${package}_LIBRARIES ${package}_LIBRARIES-NOTFOUND)
+            SET(${package}_LIBRARY ${package}_LIBRARY-NOTFOUND)
             FIND_PACKAGE(${package} QUIET)
             SET(CMAKE_INCLUDE_PATH "")
             SET(CMAKE_LIBRARY_PATH "")
