12c0e310a82849c0c3d162d8fd7872b9e0d83cb2
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2009-11-02 19:00:20 +0100

    improved syntax of testOrDelete.bat

--- config/testOrDelete.bat
+++ config/testOrDelete.bat
@@ -1,13 +1,13 @@
 set PROG=%1
 :extend_path
 shift
-if NOT "%1" == "" (
+if [%1] NEQ [] (
     set PATH=%1;%PATH%
     goto extend_path
 )
 %PROG%
 echo off
-if ERRORLEVEL 1 (
+if %ERRORLEVEL%==1 (
     del %PROG%
     exit 1 
 )
