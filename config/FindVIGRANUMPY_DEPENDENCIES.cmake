43e8f140159277c7540053e3ff5729c168996625
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2009-11-02 19:01:06 +0100

    made nosetests call more portable

--- config/FindVIGRANUMPY_DEPENDENCIES.cmake
+++ config/FindVIGRANUMPY_DEPENDENCIES.cmake
@@ -16,13 +16,13 @@ IF(PYTHONINTERP_FOUND)
     SET(VIGRANUMPY_INSTALL_DIR ${VIGRANUMPY_INSTALL_DIR}
         CACHE PATH "where to install the VIGRA Python package" FORCE)
 
-    MESSAGE(STATUS "Searching for numpy")
     execute_process ( COMMAND ${PYTHON_EXECUTABLE} -c 
                      "from numpy.distutils.misc_util import *; print ' '.join(get_numpy_include_dirs())" 
                       RESULT_VARIABLE PYTHON_NUMPY_NOT_FOUND
                       OUTPUT_VARIABLE PYTHON_NUMPY_INCLUDE_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
 
     IF(NOT PYTHON_NUMPY_NOT_FOUND)
+        MESSAGE(STATUS "Searching for numpy: ok")
         FILE(TO_CMAKE_PATH ${PYTHON_NUMPY_INCLUDE_DIR} PYTHON_NUMPY_INCLUDE_DIR)
     ELSE()
         MESSAGE(STATUS "Could NOT find Python numpy module")
@@ -32,21 +32,16 @@ IF(PYTHONINTERP_FOUND)
                      "import sys; print sys.platform" 
                       OUTPUT_VARIABLE PYTHON_PLATFORM OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-    MESSAGE(STATUS "Searching for nosetests")
-    STRING(REGEX REPLACE "/[^/]*$" "" NOSETESTS_SEARCH_DIR ${PYTHON_EXECUTABLE})
-    SET(NOSETESTS_SEARCH_DIR ${NOSETESTS_SEARCH_DIR} ${NOSETESTS_SEARCH_DIR}/Scripts)
-    find_program(NOSETESTS_EXECUTABLE nosetests
-                 PATHS ${NOSETESTS_SEARCH_DIR} ENV PATH
-                 DOC "nosetests program (required for Python testing)")
-                 
-    IF(NOT NOSETESTS_EXECUTABLE)
-        MESSAGE(STATUS "Could NOT find nosetests executable")
-    ELSEIF(MSVC OR MINGW)
-        IF(NOT NOSETESTS_EXECUTABLE MATCHES ".exe$" AND NOT NOSETESTS_EXECUTABLE MATCHES ".bat$")
-            SET(NOSETESTS_EXECUTABLE "${PYTHON_EXECUTABLE} ${NOSETESTS_EXECUTABLE}")
-        ENDIF()
+    execute_process ( COMMAND ${PYTHON_EXECUTABLE} -c 
+                     "import nose" 
+                      RESULT_VARIABLE PYTHON_NOSETESTS_NOT_FOUND)
+
+    IF(NOT PYTHON_NOSETESTS_NOT_FOUND)
+        MESSAGE(STATUS "Searching for nosetests: ok")
+    ELSE()
+        MESSAGE(STATUS "Could NOT find Python nosetests module")
     ENDIF()
-    
+
     # handle the QUIETLY and REQUIRED arguments and set VIGRANUMPY_DEPENDENCIES_FOUND to TRUE if 
     # all listed variables are TRUE
     INCLUDE(FindPackageHandleStandardArgs)
