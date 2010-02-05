94b87003e229663fa681a35a3b39de3ef1c054fa
nathan.huesken@iwr.uni-heidelberg.de <nathan.huesken@iwr.uni-heidelberg.de>
2010-02-05 10:31:54 +0100

    documentation is build correctly before vigranumpy is installed

--- vigranumpy/docsrc/conf.py
+++ vigranumpy/docsrc/conf.py
@@ -13,6 +13,10 @@
 
 import sys, os
 
+new_path=os.getcwd()+r'/../src'
+sys.path.insert(0,new_path)
+print new_path
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
