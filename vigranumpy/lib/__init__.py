988ecf89252fae7d6fe2115650772f03bdd72460
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2010-02-25 18:17:21 +0100

    added vigra.imshow()

--- vigranumpy/lib/__init__.py
+++ vigranumpy/lib/__init__.py
@@ -41,3 +41,23 @@ def searchfor(searchstring):
       for cont in contents:
          if ( cont.upper().find(searchstring.upper()) ) >= 0:
             print attr+"."+cont
+
+def imshow(image):
+    import matplotlib
+    
+    if image.ndim == 3:
+        if image.shape[2] != 3:
+            raise RuntimeError("vigra.imshow(): Multi channel image must have 3 channels.")
+        if image.dtype != uint8:
+            mi, ma = image.min(), image.max()
+            if mi >= ma:
+                image = image.__class__(image.shape, dtype=uint8)
+            else:
+                image = (image - mi) / (ma - mi)
+        return matplotlib.pyplot.imshow(image.swapaxes(0,1))
+    elif image.ndim == 2:
+        return matplotlib.pyplot.imshow(image.swapaxes(0,1), cmap=matplotlib.cm.gray, \
+                                     norm=matplotlib.cm.colors.Normalize())
+    else:
+        raise RuntimeError("vigra.imshow(): ndim must be 2 or 3.")
+
