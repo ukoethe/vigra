895c08cfcb8959a7c16a330f8cffb08cced115ce
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2010-03-21 12:55:06 +0100

    vigranumpy.index: added '#if HasHDF5'

--- vigranumpy/src/core/vigranumpyimpex.cxx
+++ vigranumpy/src/core/vigranumpyimpex.cxx
@@ -313,6 +313,8 @@ void writeVolume(NumpyArray<3, Multiband<T> > const & volume,
 
 VIGRA_PYTHON_MULTITYPE_FUNCTOR(pywriteVolume, writeVolume)
 
+#if HasHDF5
+
 namespace detail {
 template <class T>
 NumpyAnyArray readImageHDF5Impl(HDF5ImportInfo const & info)
@@ -476,47 +478,54 @@ void writeVolumeToHDF5(NumpyArray<4, Multiband<T> > const & volume,
 
 VIGRA_PYTHON_MULTITYPE_FUNCTOR(pywriteVolumeToHDF5, writeVolumeToHDF5)
 
+#endif // HasHDF5
+
 void defineImpexFunctions()
 {
     using namespace python;
     
   def("readVolume", &readVolume, (arg("filename"), arg("dtype") = "FLOAT"),
-		"Read a 3D volume from a directory\n\n"
-        "readVolume(filename, dtype = 'FLOAT') -> Volume\n\n"
+		"Read a 3D volume from a directory::\n\n"
+        "   readVolume(filename, dtype = 'FLOAT') -> Volume\n\n"
 		"If the volume is stored in a by-slice manner (e.g. one image per\n"
-		"slice), the filename can refer to an arbitrary image from the set.\n"
-		"readVolume() then assumes that the slices are enumerated like\n"
-		"name_base+[0-9]+name_ext, where name_base, the index, and name_ext\n"
+		"slice), the 'filename' can refer to an arbitrary image from the set.\n"
+		"readVolume() then assumes that the slices are enumerated like::\n\n"
+		"   name_base+[0-9]+name_ext\n\n"
+        "where name_base, the index, and name_ext\n"
 		"are determined automatically. All slice files with the same name base\n"
 		"and extension are considered part of the same volume. Slice numbers\n"
 		"must be non-negative, but can otherwise start anywhere and need not\n"
 		"be successive. Slices will be read in ascending numerical (not\n"
 		"lexicographic) order. All slices must have the same size.\n"
-		"Otherwise, readVolume() will try to read filename as an info text\n"
-		"file with the following key-value pairs:\n"
         "\n"
-        "name = [short descriptive name of the volume] (optional)\n"
-		"filename = [absolute or relative path to raw voxel data file] (required)\n"
-        "gradfile =  [abs. or rel. path to gradient data file] (currently ignored)\n"
-        "description =  [arbitrary description of the data set] (optional)\n"
-        "width = [positive integer] (required)\n"
-        "height = [positive integer] (required)\n"
-        "depth = [positive integer] (required)\n"
-        "datatype = [UNSIGNED_CHAR | UNSIGNED_BYTE] (default: UNSIGNED_CHAR)\n"
+		"Otherwise, readVolume() will try to read 'filename' as an info text\n"
+		"file with the following key-value pairs::\n"
         "\n"
-        "The voxel type is currently assumed to be binary compatible to the\n"
-		"value_type T of the MuliArray. Lines starting with # are ignored.\n"
+        "    name = [short descriptive name of the volume] (optional)\n"
+		"    filename = [absolute or relative path to raw voxel data file] (required)\n"
+        "    gradfile =  [abs. or rel. path to gradient data file] (currently ignored)\n"
+        "    description =  [arbitrary description of the data set] (optional)\n"
+        "    width = [positive integer] (required)\n"
+        "    height = [positive integer] (required)\n"
+        "    depth = [positive integer] (required)\n"
+        "    datatype = [UNSIGNED_CHAR | UNSIGNED_BYTE] (default: UNSIGNED_CHAR)\n"
+        "\n"
+        "Lines starting with # are ignored.\n"
         "When import_type is 'UINT8', 'INT16', 'UINT16', 'INT32', 'UINT32',\n"
         "'FLOAT', 'DOUBLE', or one of the corresponding numpy dtypes (numpy.uint8\n"
         "etc.), the returned volume will have the requested pixel type. For details\n"
-		"see the help for readImage.\n");
+		"see the help for :func:`readImage`.\n");
     multidef("writeVolume", pywriteVolume<Int8, UInt64, Int64, UInt16, Int16, UInt32, Int32, double, float, UInt8>(), 
        (arg("volume"), arg("filename_base"), arg("filename_ext"), arg("dtype") = "", arg("compression") = ""),
-	   "\n");
+	   "Wrtie a volume as a sequence of images::\n\n"
+       "   writeVolume(volume, filename_base, filename_ext, dtype = '', compression = '')\n\n"
+       "The resulting image sequence will be enumerated in the form::\n\n"
+       "    filename_base+[0-9]+filename_ext\n\n"
+       "Parameters 'dtype' and 'compression' will be handled as in :func:`writeImage`.\n\n");
     
     def("readImage", &readImage, (arg("filename"), arg("dtype") = "FLOAT"),
-        "Read an image from a file\n\n"
-        "readImage(filename, dtype = 'FLOAT') -> Image\n\n"
+        "Read an image from a file::\n\n"
+        "   readImage(filename, dtype = 'FLOAT') -> Image\n\n"
         "When import_type is 'UINT8', 'INT16', 'UINT16', 'INT32', 'UINT32',\n"
         "'FLOAT', 'DOUBLE', or one of the corresponding numpy dtypes (numpy.uint8\n"
         "etc.), the returned image will have the requested pixel type. If\n"
@@ -528,101 +537,137 @@ void defineImpexFunctions()
         "Supported file formats are listed by the function vigra.impexListFormats().\n"
         "When filename does not refer to a recognized image file format, an\n"
         "exception is raised. The file can be checked beforehand with the function\n"
-        "vigra.isImage(filename).\n");
+        ":func:`isImage`(filename).\n");
     multidef("writeImage", pywriteImage<Int8, UInt64, Int64, UInt16, Int16, UInt32, Int32, double, float, UInt8>(), 
        (arg("image"), arg("filename"), arg("dtype") = "", arg("compression") = ""),
-            "Save an image to a file\n\n"
-            "writeImage(image, filename, dtype = '', compression = '')\n\n"
-            "Parameters:\n\n"
-            " image:\n"
-            "    the image to be saved\n"
-            " filename:\n"
-            "    the file name to save to. The file type will be deduced\n"
-            "    from the file name extension (see vigra.impexListExtensions()\n"
-            "    for a list of supported extensions).\n"
-            " dtype:\n"
-            "    the pixel type written to the file. Possible values:\n\n"
-            "     '' or 'NATIVE':\n"
-            "        save with original pixel type, or convert automatically\n"
-            "        when this type is unsupported by the target file format\n"
-            "     'UINT8', 'INT16', 'UINT16', 'INT32', 'UINT32', 'FLOAT', 'DOUBLE':\n"
-            "        save as specified, or raise exception when this type is not \n"
-            "        supported by the target file format (see list below)\n"
-            "     'NBYTE':\n"
-            "        normalize to range 0...255 and then save as 'UINT8'\n"
-            "     numpy.uint8, numpy.int16 etc.:\n"
-            "        behaves like the corresponding string argument\n\n" 
-            " compression:\n"
-            "     how to compress the data (ignored when compression type is unsupported \n"
-            "     by the file format). Possible values:\n\n"
-            "     '' or not given:\n"
-            "        save with the native compression of the target file format\n"
-            "     'RLE', 'RunLength':\n"
-            "        use run length encoding (native in BMP, supported by TIFF)\n"
-            "     'DEFLATE':\n"
-            "        use deflate encoding (only supported by TIFF)\n"
-            "     'LZW':\n"
-            "        use LZW algorithm (only supported by TIFF with LZW enabled)\n"
-            "     'ASCII':\n"
-            "        write as ASCII rather than binary file (only supported by PNM)\n"
-            "     '1' ... '100':\n"
-            "        use this JPEG compression level (only supported by JPEG and TIFF)\n\n"
-            "Supported file formats are listed by the function vigra.impexListFormats().\n"
-            "The different file formats support the following pixel types:\n\n"
-            "   BMP:\n"
-            "       Microsoft Windows bitmap image file (pixel type: UINT8 as gray and RGB).\n" 
-            "   GIF:\n"
-            "       CompuServe graphics interchange format; 8-bit color\n"
-            "       (pixel type: UINT8 as gray and RGB).\n" 
-            "   JPEG:\n"
-            "       Joint Photographic Experts Group JFIF format; compressed 24-bit color\n"
-            "       (pixel types: UINT8 as gray and RGB). (only available if libjpeg is installed)\n" 
-            "   PNG:\n"
-            "       Portable Network Graphic (pixel types: UINT8 and UINT16 with\n"
-            "       up to 4 channels). (only available if libpng is installed)\n"
-            "   PBM:\n"
-            "       Portable bitmap format (black and white).\n" 
-            "   PGM:\n"
-            "       Portable graymap format (pixel types: UINT8, INT16, INT32 as gray scale)).\n" 
-            "   PNM:\n"
-            "       Portable anymap (pixel types: UINT8, INT16, INT32, gray and RGB)\n" 
-            "   PPM:\n"
-            "       Portable pixmap format (pixel types: UINT8, INT16, INT32 as RGB)\n"
-            "   SUN:\n"
-            "       SUN Rasterfile (pixel types: UINT8 as gray and RGB).\n" 
-            "   TIFF:\n"
-            "       Tagged Image File Format (pixel types: UINT8, INT16, INT32, FLOAT, DOUBLE\n"
-            "       with up to 4 channels). (only available if libtiff is installed.)\n"
-            "   VIFF:\n"
-            "       Khoros Visualization image file (pixel types: UINT8, INT16\n"
-            "       INT32, FLOAT, DOUBLE with arbitrary many channels).\n\n");
+        "Save an image to a file::\n\n"
+        "   writeImage(image, filename, dtype = '', compression = '')\n\n"
+        "Parameters:\n\n"
+        " image:\n"
+        "    the image to be saved\n"
+        " filename:\n"
+        "    the file name to save to. The file type will be deduced\n"
+        "    from the file name extension (see vigra.impexListExtensions()\n"
+        "    for a list of supported extensions).\n"
+        " dtype:\n"
+        "    the pixel type written to the file. Possible values:\n\n"
+        "     '' or 'NATIVE':\n"
+        "        save with original pixel type, or convert automatically\n"
+        "        when this type is unsupported by the target file format\n"
+        "     'UINT8', 'INT16', 'UINT16', 'INT32', 'UINT32', 'FLOAT', 'DOUBLE':\n"
+        "        save as specified, or raise exception when this type is not \n"
+        "        supported by the target file format (see list below)\n"
+        "     'NBYTE':\n"
+        "        normalize to range 0...255 and then save as 'UINT8'\n"
+        "     numpy.uint8, numpy.int16 etc.:\n"
+        "        behaves like the corresponding string argument\n\n" 
+        " compression:\n"
+        "     how to compress the data (ignored when compression type is unsupported \n"
+        "     by the file format). Possible values:\n\n"
+        "     '' or not given:\n"
+        "        save with the native compression of the target file format\n"
+        "     'RLE', 'RunLength':\n"
+        "        use run length encoding (native in BMP, supported by TIFF)\n"
+        "     'DEFLATE':\n"
+        "        use deflate encoding (only supported by TIFF)\n"
+        "     'LZW':\n"
+        "        use LZW algorithm (only supported by TIFF with LZW enabled)\n"
+        "     'ASCII':\n"
+        "        write as ASCII rather than binary file (only supported by PNM)\n"
+        "     '1' ... '100':\n"
+        "        use this JPEG compression level (only supported by JPEG and TIFF)\n\n"
+        "Supported file formats are listed by the function vigra.impexListFormats().\n"
+        "The different file formats support the following pixel types:\n\n"
+        "   BMP:\n"
+        "       Microsoft Windows bitmap image file (pixel type: UINT8 as gray and RGB).\n" 
+        "   GIF:\n"
+        "       CompuServe graphics interchange format; 8-bit color\n"
+        "       (pixel type: UINT8 as gray and RGB).\n" 
+        "   JPEG:\n"
+        "       Joint Photographic Experts Group JFIF format; compressed 24-bit color\n"
+        "       (pixel types: UINT8 as gray and RGB). (only available if libjpeg is installed)\n" 
+        "   PNG:\n"
+        "       Portable Network Graphic (pixel types: UINT8 and UINT16 with\n"
+        "       up to 4 channels). (only available if libpng is installed)\n"
+        "   PBM:\n"
+        "       Portable bitmap format (black and white).\n" 
+        "   PGM:\n"
+        "       Portable graymap format (pixel types: UINT8, INT16, INT32 as gray scale)).\n" 
+        "   PNM:\n"
+        "       Portable anymap (pixel types: UINT8, INT16, INT32, gray and RGB)\n" 
+        "   PPM:\n"
+        "       Portable pixmap format (pixel types: UINT8, INT16, INT32 as RGB)\n"
+        "   SUN:\n"
+        "       SUN Rasterfile (pixel types: UINT8 as gray and RGB).\n" 
+        "   TIFF:\n"
+        "       Tagged Image File Format (pixel types: UINT8, INT16, INT32, FLOAT, DOUBLE\n"
+        "       with up to 4 channels). (only available if libtiff is installed.)\n"
+        "   VIFF:\n"
+        "       Khoros Visualization image file (pixel types: UINT8, INT16\n"
+        "       INT32, FLOAT, DOUBLE with arbitrary many channels).\n\n");
     def("impexListFormats", &impexListFormats, 
-        "impexListFormats() -> string\n\n"
+        "Ask for the image file formats that vigra.impex understands::\n\n"
+        "    impexListFormats() -> string\n\n"
         "This function returns a string containing the supported image file "
-        "formats for reading and writing with the functions readImage and "
-        "writeImage.\n");
+        "formats for reading and writing with the functions :func:`readImage` and "
+        ":func:`writeImage`.\n");
     def("impexListExtensions", &impexListExtensions,
-        "impexListExtensions() -> string\n\n"
+        "Ask for the image file extensions that vigra.impex understands::\n\n"
+        "    impexListExtensions() -> string\n\n"
         "This function returns a string containing the supported image file "
-        "extensions for reading and writing with the functions readImage and "
-        "writeImage.\n");
+        "extensions for reading and writing with the functions :func:`readImage` and "
+        ":func:`writeImage`.\n");
     def("isImage", &isImage, args("filename"),
-        "isImage(filename) -> bool\n\n"
+        "Check whether the given file name contains image data::\n\n"
+        "   isImage(filename) -> bool\n\n"
         "This function tests whether a file has a supported image format. "
         "It checks the first few bytes of the file and compares them with "
         "the \"magic strings\" of each recognized image format. If the "
         "image format is supported it returns True otherwise False.\n");
 
+#if HasHDF5
 	def("readImageFromHDF5", &readImageFromHDF5, (arg("filepath"), arg("pathInFile"), arg("dtype") = "FLOAT"),
-        "Read an image from a file\n\n");
+        "Read an image from a HDF5 file::\n\n"
+        "   readImageFromHDF5(filepath, pathInFile, dtype='FLOAT') -> image\n\n"
+        "If the file contains 3-dimensional data, the innermost (last)\n"
+        "index is interpreted as a channel dimension.\n\n");
 	def("readVolumeFromHDF5", &readVolumeFromHDF5, (arg("filepath"), arg("pathInFile"), arg("dtype") = "FLOAT"),
-        "Read a volume from a file\n\n");
+        "Read a volume from a file::\n\n"
+        "   readVolumeFromHDF5(filepath, pathInFile, dtype='FLOAT') -> volume\n\n"
+        "If the file contains 4-dimensional data, the innermost (last)\n"
+        "index is interpreted as a channel dimension.\n\n");
 	multidef("writeImageToHDF5", pywriteImageToHDF5<Int8, UInt64, Int64, UInt16, Int16, UInt32, Int32, double, float, UInt8>(), 
        (arg("image"), arg("filepath"), arg("pathInFile"), arg("dtype") = ""),
-            "Save an image to an HDF5 file\n\n");
+        "Save an image to an HDF5 file::\n\n"
+        "   writeImageToHDF5(image, filepath, pathInFile, dtype='')\n\n"
+        "When dtype is not given (the default), date are written in their native type.\n"
+        "The resulting HDF5 dataset should be identical to the one created by\n"
+        "the Python module `h5py <http://h5py.alfven.org/>`_ as follows::\n\n"
+        "   import vigra, h5py\n"
+        "   image = vigra.readImage(filename)\n"
+        "   vigra.writeImageToHDF5(image, 'vigra_export.h5', 'MyImage')\n"
+        "   h5py_file = h5py.File('h5py_export.h5', 'w')\n"
+        "   h5py_file.create_dataset('MyImage', data=image.swapaxes(0, 1))\n"
+        "   h5py_file.close()\n"
+        "\n"
+        "(note the axes transposition which accounts for the VIGRA indexing convention).\n\n");
     multidef("writeVolumeToHDF5", pywriteVolumeToHDF5<Int8, UInt64, Int64, UInt16, Int16, UInt32, Int32, double, float, UInt8>(), 
        (arg("volume"), arg("filepath"), arg("pathInFile"), arg("dtype") = ""),
-            "Save a volume to an HDF5 file\n\n");
+        "Save a volume to an HDF5 file::\n\n"
+        "   writeVolumeToHDF5(volume, filepath, pathInFile, dtype='')\n\n"
+        "When dtype is not given (the default), date are written in their native type.\n"
+        "The resulting HDF5 dataset should be identical to the one created by\n"
+        "the Python module `h5py <http://h5py.alfven.org/>`_ as follows::\n\n"
+        "   import vigra, h5py\n"
+        "   volume = vigra.readVolume(filename)\n"
+        "   vigra.writeVolumeToHDF5(volume, 'vigra_export.h5', 'MyVolume')\n"
+        "   h5py_file = h5py.File('h5py_export.h5', 'w')\n"
+        "   h5py_file.create_dataset('MyVolume', data=volume.swapaxes(0, 2))\n"
+        "   h5py_file.close()\n"
+        "\n"
+        "(note the axes transposition which accounts for the VIGRA indexing convention).\n\n");
+
+#endif // HasHDF5
 }
 
 } // namespace vigra
