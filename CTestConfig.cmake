605b61beec252aa8d8477951ca8ef42d906140fd
sschmid2@rubens.hci.uni-heidelberg.de <sschmid2@rubens.hci.uni-heidelberg.de>
2010-04-06 18:52:44 +0200

    Enable CTest test submissions to CDash dashboard (preliminary)

---
+++ CTestConfig.cmake
@@ -0,0 +1,10 @@
+set(CTEST_PROJECT_NAME "Vigra")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+
+
+set(CTEST_DROP_METHOD "http")
+
+set(CTEST_DROP_SITE "rubens")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=Vigra")
+set(CTEST_DROP_SITE_CDASH TRUE)
