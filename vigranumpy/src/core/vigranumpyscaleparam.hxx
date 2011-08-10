e84810780c6e0cfe9773ed22662d26f1b94e5e52
Markus Nullmeier <markus.nullmeier@telu.de>
2011-08-10 11:39:02 +0200

    more indentation fixes for multi_convolution

--- vigranumpy/src/core/vigranumpyscaleparam.hxx
+++ vigranumpy/src/core/vigranumpyscaleparam.hxx
@@ -17,23 +17,25 @@ struct pythonScaleParam1
     typedef TinyVector<double, ndim> p_vector;
     typedef typename p_vector::const_iterator return_type;
     p_vector vec;
-    static unsigned len_check(python::object val, const char *const function_name)
+    static unsigned len_check(python::object val,
+                              const char *const function_name)
     {
-          unsigned count = python::len(val);
-          if (count == 1)
-              return 0;
-          if (count == ndim)
-              return 1;
+        unsigned count = python::len(val);
+        if (count == 1)
+            return 0;
+        if (count == ndim)
+            return 1;
 
-          std::string msg = std::string(function_name)
-	                    + "(): Parameter number be 1 or equal to the number of spatial dimensions.";
-          PyErr_SetString(PyExc_ValueError, msg.c_str());
-          python::throw_error_already_set();
-          return 0;
+        std::string msg = std::string(function_name) + "(): Parameter number "
+                      "must be 1 or equal to the number of spatial dimensions.";
+        PyErr_SetString(PyExc_ValueError, msg.c_str());
+        python::throw_error_already_set();
+        return 0;
     }
     pythonScaleParam1()
     {}
-    pythonScaleParam1(python::object val, const char *const function_name = "pythonScaleParam1")
+    pythonScaleParam1(python::object val,
+                      const char *const function_name = "pythonScaleParam1")
     {
         if (PySequence_Check(val.ptr()))
         {
@@ -42,7 +44,7 @@ struct pythonScaleParam1
             for (unsigned i = 0; i != ndim; ++i, i_v += increment)
                 vec[i] = python::extract<double>(val[i_v]);
         }
-	else
+        else
         {
             double x = python::extract<double>(val);
             vec = p_vector(x);
@@ -63,28 +65,34 @@ struct pythonScaleParam
     pythonScaleParam1<ndim> sigma_d;
     pythonScaleParam1<ndim> step_size;
     pythonScaleParam1<ndim> outer_scale;
-    pythonScaleParam(python::object s_eff, python::object s_d, python::object s_size,
+
+    pythonScaleParam(python::object s_eff,
+                     python::object s_d,
+                     python::object s_size,
                      const char *const function_name = "pythonScaleParam")
         : sigma_eff(s_eff, function_name),
           sigma_d(s_d, function_name),
           step_size(s_size, function_name)
     {}
-    pythonScaleParam(python::object s_eff, python::object s_d, python::object s_size,
-                     python::object s_outer, const char *const function_name = "pythonScaleParam")
+    pythonScaleParam(python::object s_eff,
+                     python::object s_d,
+                     python::object s_size,
+                     python::object s_outer,
+                     const char *const function_name = "pythonScaleParam")
         : sigma_eff(s_eff, function_name),
           sigma_d(s_d, function_name),
           step_size(s_size, function_name),
-	  outer_scale(s_outer, function_name)
+          outer_scale(s_outer, function_name)
     {}
     return_type operator()() const
     {
         return_type opt;
-	return opt.stdDev(sigma_eff()).resolutionStdDev(sigma_d())
-	          .stepSize(step_size()).outerScale(outer_scale());
+        return opt.stdDev(sigma_eff()).resolutionStdDev(sigma_d())
+                  .stepSize(step_size()).outerScale(outer_scale());
     }
 };
 
-	
+
 } // namespace vigra
 
 #endif // VIGRANUMPYSCALEPARAM_HXX
