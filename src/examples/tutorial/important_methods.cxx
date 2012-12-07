1db5c6f49f186ae4c76f571d1790c6fd2124a474
Patrick Fahner <Fahner@stud.uni-heidelberg.de>
2012-12-07 11:34:15 +0100

    tutorial: subarray added

--- src/examples/tutorial/important_methods.cxx
+++ src/examples/tutorial/important_methods.cxx
@@ -21,7 +21,14 @@ int main (int argc, char ** argv) {
     // initialize new array with 3rd column of intArray
     vigra::MultiArray<1, int> lowArray = intArray.bind<0>(2);
 
-    for (iter = lowArray.begin(); iter != lowArray.end(); iter++) {
+    // copy the upper-left subsquare of a 4x4-matrix
+    vigra::MultiArray<2, int> _44Matrix(Shape2(4,4));
+    _44Matrix.init(1);
+    vigra::MultiArray<2, int> subsquare = _44Matrix.subarray(Shape2(0,0), Shape2(2,2));
+
+    // change the elements of a subarray
+    _44Matrix.subarray(Shape2(0,2),Shape2(4,4)) = 0;
+    for (iter = _44Matrix.begin(); iter != _44Matrix.end(); iter++) {
 		std::cout << *iter << "  ";
 	}
     std::cout << std::endl;
---
+++ src/examples/tutorial/subimage_tutorial.cxx
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vigra/multi_array.hxx>
+#include <vigra/stdimage.hxx>
+#include <vigra/impex.hxx>
+
+using namespace vigra; 
+
+
+int main(int argc, char ** argv) {
+    if(argc != 3) {
+        std::cout << "Usage: " << argv[0] << " infile outfile" << std::endl;
+        std::cout << "(supported formats: " << vigra::impexListFormats() << ")" << std::endl;
+        
+        return 1;
+    }
+
+    try {
+        // read image given as first argument
+        // file type is determined automatically
+        vigra::ImageImportInfo info(argv[1]);
+        
+        if(info.isGrayscale()) {
+            
+            // write image data to MultiArray
+			vigra::MultiArray<2, unsigned char> imageArray(Shape2(info.width(), info.height()));
+			importImage(info, destImage(imageArray));
+
+            // calculate upper-left (x0,y0) and lower-right (x1,y1) position of subimage
+            int x0 = info.width()/4;
+            int y0 = info.height()/4;
+            int x1 = info.width()-x0;
+            int y1 = info.height()-y0;
+
+            // copy subimage into new array for output
+            vigra::MultiArray<2, unsigned char> newImageArray = 
+                    imageArray.subarray(Shape2(x0,y0), Shape2(x1, y1));
+            
+            // write the image to the file given as second argument
+            // the file type will be determined from the file name's extension
+            exportImage(srcImageRange(newImageArray), vigra::ImageExportInfo(argv[2]));
+        }
+        else {
+            // write image data to MultiArray
+			vigra::MultiArray<2, vigra::RGBValue<unsigned char> > imageArray(Shape2(info.width(), info.height()));
+			importImage(info, destImage(imageArray));
+
+            // calculate upper-left (x0,y0) and lower-right (x1,y1) position of subimage
+            int x0 = info.width()/4;
+            int y0 = info.height()/4;
+            int x1 = info.width()-x0;
+            int y1 = info.height()-y0;
+
+            // copy subimage into new array for output
+            vigra::MultiArray<2, vigra::RGBValue<unsigned char> > newImageArray = 
+                    imageArray.subarray(Shape2(x0,y0), Shape2(x1, y1));
+            
+            // write the image to the file given as second argument
+            // the file type will be determined from the file name's extension
+            exportImage(srcImageRange(newImageArray), vigra::ImageExportInfo(argv[2]));
+        }
+    }
+    catch (vigra::StdException & e) {
+        // catch any errors that might have occurred and print their reason
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+    
+    return 0;
+}
