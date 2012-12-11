b2578c60adea52780581bf2ea219369aeb136279
Patrick Fahner <Fahner@stud.uni-heidelberg.de>
2012-12-11 17:37:49 +0100

    tutorial: ImageIO added

---
+++ src/examples/tutorial/imageImportInfo_tutorial.cxx
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "vigra/impex.hxx"
+
+using namespace vigra; 
+
+
+int main(int argc, char ** argv) {
+    
+    // read image given as first argument
+    vigra::ImageImportInfo imageInfo(argv[1]);
+
+    // Print some information:
+    std::cout << "Image information:\n";
+    std::cout << "Filetype: " << imageInfo.getFileType() << std::endl;
+    std::cout << "Width: " << imageInfo.width() << std::endl;
+    std::cout << "Height: " << imageInfo.height() << std::endl;
+    std::cout << "Color Image: ";
+    if (imageInfo.isColor())    std::cout << "yes\n";
+    else                        std::cout << "no\n";
+
+    return 0;
+}
