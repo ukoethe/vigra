87ee87988e3de16120bd4550275ed0d5451c68f6
Patrick Fahner <Fahner@stud.uni-heidelberg.de>
2013-01-29 13:56:03 +0100

    tutorial: smooth convolve for color images

--- src/examples/tutorial/smooth_convolve.cxx
+++ src/examples/tutorial/smooth_convolve.cxx
@@ -54,7 +54,42 @@ int main (int argc, char ** argv) {
     // process color image
     else {
        
-        std::cout << "no grayscale image!\n";
+        // instantiate arrays for image data and for smoothed image of appropriate size
+        vigra::MultiArray<2, vigra::RGBValue<float> > imageArray(info.shape());
+        vigra::MultiArray<2, vigra::RGBValue<float> > exportArray(info.shape());
+
+        // copy image data into array
+        importImage(info, destImage(imageArray));
+
+        // instantiate convolving kernel
+        vigra::Kernel2D<double> kernel;
+        
+        // choose of convolving mode
+        std::cout << "Which mode of convolution?\n";
+        std::cout << "1 - 3x3-average\n";
+        std::cout << "2 - 3x3-box just corners\n";
+        std::cout << "3 - gaussian\n";
+        int mode;
+        std::cin >> mode;
+        
+        // initializing kernel       
+        switch(mode) {
+            // homogenous averaging of 3x3-box
+            case 1: kernel.initExplicitly(Diff2D(-1,-1), Diff2D(1,1)) = 1.0/9.0; break;
+            // averaging corners of 3x3-box
+            case 2: kernel.initExplicitly(Diff2D(-1,-1), Diff2D(1,1)) = 
+                        0.25, 0.0, 0.25, 
+                        0.0,  0.0, 0.0,
+                        0.25, 0.0, 0.25; break;
+            // gaussian convolving
+            case 3: kernel.initGaussian(1.5); break;
+        }
+       
+        // convolve image
+        convolveImage(srcImageRange(imageArray), destImage(exportArray), kernel2d(kernel));
+
+        // write image data to the file given as second argument
+        exportImage(srcImageRange(exportArray), vigra::ImageExportInfo(argv[2]));
         
     }
     return 0;
