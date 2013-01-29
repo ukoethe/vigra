85bceec9ae4dc372c35149e32ac8d3d642b59740
Patrick Fahner <Fahner@stud.uni-heidelberg.de>
2013-01-29 15:32:29 +0100

    tutorial: separableConvole added

--- src/examples/tutorial/smooth_convolve.cxx
+++ src/examples/tutorial/smooth_convolve.cxx
@@ -21,31 +21,40 @@ int main (int argc, char ** argv) {
         importImage(info, destImage(imageArray));
 
         // instantiate convolving kernel
-        vigra::Kernel2D<float> kernel;
+        vigra::Kernel2D<double> kernel2dim;
+        vigra::Kernel1D<double> kernel1dim;
         
         // choose of convolving mode
         std::cout << "Which mode of convolution?\n";
         std::cout << "1 - 3x3-average\n";
         std::cout << "2 - 3x3-box just corners\n";
         std::cout << "3 - gaussian\n";
+        std::cout << "4 - X- and Y-dimension separately\n";
         int mode;
         std::cin >> mode;
         
         // initializing kernel       
         switch(mode) {
             // homogenous averaging of 3x3-box
-            case 1: kernel.initExplicitly(Diff2D(-1,-1), Diff2D(1,1)) = 1.0/9.0; break;
+            case 1: kernel2dim.initExplicitly(Diff2D(-1,-1), Diff2D(1,1)) = 1.0/9.0; break;
             // averaging corners of 3x3-box
-            case 2: kernel.initExplicitly(Diff2D(-1,-1), Diff2D(1,1)) = 
+            case 2: kernel2dim.initExplicitly(Diff2D(-1,-1), Diff2D(1,1)) = 
                         0.25, 0.0, 0.25, 
                         0.0,  0.0, 0.0,
                         0.25, 0.0, 0.25; break;
             // gaussian convolving
-            case 3: kernel.initGaussian(1.5); break;
+            case 3: kernel2dim.initGaussian(1.5); break;
+            case 4: kernel1dim.initExplicitly(-1,1) = 1.0/3.0; break;
         }
        
         // convolve image
-        convolveImage(srcImageRange(imageArray), destImage(exportArray), kernel2d(kernel));
+        if (mode == 4) {
+             separableConvolveX(srcImageRange(imageArray), destImage(imageArray), kernel1d(kernel1dim));
+             separableConvolveY(srcImageRange(imageArray), destImage(exportArray), kernel1d(kernel1dim));
+        }
+        else {
+            convolveImage(srcImageRange(imageArray), destImage(exportArray), kernel2d(kernel2dim));
+        }
 
         // write image data to the file given as second argument
         exportImage(srcImageRange(exportArray), vigra::ImageExportInfo(argv[2]));
@@ -62,31 +71,40 @@ int main (int argc, char ** argv) {
         importImage(info, destImage(imageArray));
 
         // instantiate convolving kernel
-        vigra::Kernel2D<double> kernel;
+        vigra::Kernel2D<double> kernel2dim;
+        vigra::Kernel1D<double> kernel1dim;
         
         // choose of convolving mode
         std::cout << "Which mode of convolution?\n";
         std::cout << "1 - 3x3-average\n";
         std::cout << "2 - 3x3-box just corners\n";
         std::cout << "3 - gaussian\n";
+        std::cout << "4 - X- and Y-dimension separately\n";
         int mode;
         std::cin >> mode;
         
         // initializing kernel       
         switch(mode) {
             // homogenous averaging of 3x3-box
-            case 1: kernel.initExplicitly(Diff2D(-1,-1), Diff2D(1,1)) = 1.0/9.0; break;
+            case 1: kernel2dim.initExplicitly(Diff2D(-1,-1), Diff2D(1,1)) = 1.0/9.0; break;
             // averaging corners of 3x3-box
-            case 2: kernel.initExplicitly(Diff2D(-1,-1), Diff2D(1,1)) = 
+            case 2: kernel2dim.initExplicitly(Diff2D(-1,-1), Diff2D(1,1)) = 
                         0.25, 0.0, 0.25, 
                         0.0,  0.0, 0.0,
                         0.25, 0.0, 0.25; break;
             // gaussian convolving
-            case 3: kernel.initGaussian(1.5); break;
+            case 3: kernel2dim.initGaussian(1.5); break;
+            case 4: kernel1dim.initExplicitly(-1,1) = 1.0/3.0; break;
         }
        
         // convolve image
-        convolveImage(srcImageRange(imageArray), destImage(exportArray), kernel2d(kernel));
+        if (mode == 4) {
+             separableConvolveX(srcImageRange(imageArray), destImage(imageArray), kernel1d(kernel1dim));
+             separableConvolveY(srcImageRange(imageArray), destImage(exportArray), kernel1d(kernel1dim));
+        }
+        else {
+            convolveImage(srcImageRange(imageArray), destImage(exportArray), kernel2d(kernel2dim));
+        }
 
         // write image data to the file given as second argument
         exportImage(srcImageRange(exportArray), vigra::ImageExportInfo(argv[2]));
