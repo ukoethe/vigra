d84ac3f0bd5914d741ba52191ce7244022ba60c4
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2014-02-03 11:42:45 +0100

    wip

--- include/vigra/graph_helper/degree_map.hxx
+++ include/vigra/graph_helper/degree_map.hxx
@@ -1,6 +1,8 @@
 #ifndef VIGRA_DEGREE_MAP_HXX
 #define VIGRA_DEGREE_MAP_HXX
 
+/*vigra*/ 
+#include <vigra/graphs.hxx>
 
 namespace vigra{
 namespace detail{
@@ -13,22 +15,30 @@ namespace detail{
 		typedef typename Graph::Node Node;
 		typedef typename Graph::index_type index_type;
 
+		typedef Node Key;
+		typedef index_type Value;
+		typedef index_type ConstReference;
+		
+
 		DegreeMap(const Graph graph)
 		: graph_(graph){
 		}
 		index_type operator[](const Node & node)const{
-			return graph_.degree(node);
+			// use the boost free function
+			// => will work with all our graphs
+			return boost::degree(node,*graph_);
 		}
 		index_type operator[](const Node & node){
-			return graph_.degree(node);
+			// use the boost free function
+			// => will work with all our graphs
+			return boost::degree(node,*graph_);
 		}
 
 
 	private:
 
-		const Graph & graph_;
+		const Graph * graph_;
 	};	
-}
 }
 
 
