f10f42ca787268a0e6fb98a0a9e6e8fce2bff18d
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2014-02-22 12:42:03 +0100

    added future interface in an example

---
+++ vigranumpy/examples/seg_interface.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+def graphSegmentation(
+	graph,
+	nodeFeatures   = None,
+	edgeIndicators = None,
+	method,
+	**kwargs
+) :
+	
+	if method in ("fw","felzenszwalb") :
+		pass
+	if method = ("mc","multicut"):
+		pass
+	if method = ("mcw","multiwaycut"):
+		pass
+	if method = ("hc","hierarchicalClustering")
+		pass
+	if method = ("watersheds")
+		pass
