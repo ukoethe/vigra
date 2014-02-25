93d7d3822f76f80f23454802848e6ed84eb39690
Philip Schill <philip.schill@gmx.de>
2014-02-25 15:26:20 +0100

    Script for eccentricity transformation now works in 3d. Added function pyRagFindEdges (not tested, but compiling works).

--- vigranumpy/examples/eccentricity/eccentricity_transform_3dnoise.py
+++ vigranumpy/examples/eccentricity/eccentricity_transform_3dnoise.py
@@ -7,8 +7,8 @@ import sys
 
 
 gamma = 0.0001
-percentage = 2
-f = "figure_1.png"
+percentage = 100
+f = "figure_2.png"
 
 
 
@@ -20,26 +20,47 @@ f = "figure_1.png"
 ## showPathImage: if True, the image with distance transform and paths will be shown
 ## percentageOfPaths: percentage of computed paths
 def eccentricity( img, distFunc = "exponential", showPathImage = False, percentageOfPaths = 100, imgSaveName = "" ):
-    ## Enlarge image by one pixel on each side
+
     img = img.astype(numpy.uint8)
-    bigImg = numpy.ones( (img.shape[0]+2, img.shape[1]+2) )
-    bigImg[1:bigImg.shape[0]-1, 1:bigImg.shape[1]-1] = img
+    dim = len(img.shape)
 
-    ## Find borders in img (replace with graph functions)
+    ## Enlarge image by one pixel on each side
+    bigShape = []
+    for s in img.shape:
+        bigShape.append(s + 2)
+    bigImg = numpy.ones(bigShape)
+    slices = []
+    for i in range(dim):
+        slices.append(slice(1, bigImg.shape[i]-1))
+    bigImg[ slices ] = img
+
+    ## Find borders in img (TODO: replace with graph functions, maybe regionImageToCrackEdgeImage ( labelImage ) )
     borderImg = numpy.zeros(bigImg.shape)
-    for y in range(bigImg.shape[1]-1):
-        for x in range(bigImg.shape[0]-1):
-            if bigImg[x,y] == 0:
-                if bigImg[x+1,y] == 1 or bigImg[x,y+1] == 1:
-                    borderImg[x, y] = 1
-            else:
-                if bigImg[x+1,y] == 0:
-                    borderImg[x+1, y] = 1
-                if bigImg[x,y+1] == 0:
-                    borderImg[x, y+1] = 1
-
-## regionImageToCrackEdgeImage ( labelImage )
-
+    if dim == 2:
+        for y in range(bigImg.shape[1]-1):
+            for x in range(bigImg.shape[0]-1):
+                if bigImg[x,y] == 0:
+                    if bigImg[x+1,y] == 1 or bigImg[x,y+1] == 1:
+                        borderImg[x, y] = 1
+                else:
+                    if bigImg[x+1,y] == 0:
+                        borderImg[x+1, y] = 1
+                    if bigImg[x,y+1] == 0:
+                        borderImg[x, y+1] = 1
+    else:
+        for z in range(bigImg.shape[2]-1):
+            for y in range(bigImg.shape[1]-1):
+                for x in range(bigImg.shape[0]-1):
+                    if bigImg[x,y,z] == 0:
+                        if bigImg[x+1,y,z] == 1 or bigImg[x,y+1,z] == 1 or bigImg[x,y,z+1] == 1:
+                            borderImg[x, y, z] = 1
+                    else:
+                        if bigImg[x+1,y,z] == 0:
+                            borderImg[x+1,y,z] = 1
+                        if bigImg[x,y+1,z] == 0:
+                            borderImg[x,y+1,z] = 1
+                        if bigImg[x,y,z+1] == 0:
+                            borderImg[x,y,z+1] = 1
 
     # ## Apply distanceTransform and modify (outside: high values, inside: low values)
     # distImage = vigra.filters.distanceTransform2D(bigImg.astype(numpy.float32))
@@ -63,18 +84,22 @@ def eccentricity( img, distFunc = "exponential", showPathImage = False, percenta
     ## Set the outside to a very high value
     distImage[numpy.where(bigImg==1)]=10000.0
 
+    ## Image copy to draw the paths in
     imgp = distImage.copy()
+    imgp[numpy.where(bigImg==1)] = 100
 
     ## Get image graph and its path finder
-    gridGraph = vigraph.gridGraph(bigImg.shape[0:2],False)
-    edgeWeights = vigra.resize(distImage,[distImage.shape[0]*2-1,distImage.shape[1]*2-1],order=0)
+    gridGraph = vigraph.gridGraph(bigImg.shape[0:dim],False)
+    graphShape = []
+    for s in distImage.shape:
+        graphShape.append(s*2-1)
+    edgeWeights = vigra.resize(distImage, graphShape, order=0)
     edgeWeights = vigra.graphs.edgeFeaturesFromInterpolatedImageCorrected(gridGraph,edgeWeights)
     pathFinder = vigraph.ShortestPathPathDijkstra(gridGraph)
 
     ## End points for paths (all points on the border)
     targets = numpy.where(borderImg==1)
-    tx,ty = targets
-    nTargets = len(tx)
+    nTargets = len(targets[0])
 
     ## Indices of start points for paths (random)
     nPoints = int(numpy.ceil(percentageOfPaths * nTargets / 100.0))
@@ -85,11 +110,17 @@ def eccentricity( img, distFunc = "exponential", showPathImage = False, percenta
     maxPaths = []
     maxPathLengths = []
     for i in range(nPoints):
-        source = gridGraph.coordinateToNode((int(tx[starts[i]]), int (ty[starts[i]])))
+        sourceIndex = []
+        for d in range(dim):
+            sourceIndex.append(int(targets[d][starts[i]]))
+        source = gridGraph.coordinateToNode(sourceIndex)
         pathFinder.run(edgeWeights, source)
         maxPathLength = 0
         for j in range(nTargets):
-            target = gridGraph.coordinateToNode((int(tx[j]), int(ty[j])))
+            targetIndex = []
+            for d in range(dim):
+                targetIndex.append(int(targets[d][j]))
+            target = gridGraph.coordinateToNode(targetIndex)
             path = pathFinder.path(pathType='coordinates', target=target)
             pathLength = pathFinder.distance(target)
             if pathLength > maxPathLength or maxPathLength == 0:
@@ -97,30 +128,42 @@ def eccentricity( img, distFunc = "exponential", showPathImage = False, percenta
                 maxPath = path
         maxPaths.append(maxPath)
         maxPathLengths.append(maxPathLength)
+        imgp[sourceIndex[0], sourceIndex[1]] = 40*maxPathLength
 
-    if showPathImage or len(imgSaveName)>1:
+    if showPathImage:
         val = (imgp.max()+imgp.min())/2
         for p in maxPaths:
             imgp[p[:,0], p[:,1]] = val
     if showPathImage:
         plt.figure(distFunc)
-        plt.imshow(imgp, interpolation='none')
+        plt.imshow(numpy.swapaxes(imgp, 1, 0), interpolation='none')
     if len(imgSaveName)>1:
-        scipy.misc.imsave(imgSaveName, imgp)
+
+        scipy.misc.imsave(imgSaveName, numpy.swapaxes(imgp, 1, 0))
 
     return maxPathLengths
 
 
-## Read image
-img = vigra.impex.readImage(f)
-labels = numpy.squeeze(vigra.analysis.labelImage(img))
 
-### Compute slic superpixels
-#labels ,nseg = vigra.analysis.slicSuperpixels(img,100.0,50)
-#labels       = numpy.squeeze(vigra.analysis.labelImage(labels))
+loadImage = True
+if loadImage:
+    ## Read image from file
+    img = vigra.impex.readImage(f)
+    labels = numpy.squeeze(vigra.analysis.labelImage(img))
+else:
+    ## Random 3d image
+    img = numpy.ones( (100, 100, 20) )
+    img = numpy.random.rand(100, 100, 20)
+
+    ## Compute slic superpixels
+    labels ,nseg = vigra.analysis.slicSuperpixels(img.astype(numpy.float32),100.0,50)
+    labels       = numpy.squeeze(vigra.analysis.labelVolume(labels))
+
+## Dimension
+dim = len(labels.shape)
 
 ## Compute bounding boxes
-regionFeatures = vigra.analysis.extractRegionFeatures(img, labels)
+regionFeatures = vigra.analysis.extractRegionFeatures(img.astype(numpy.float32), labels)
 upperLeftBBs = regionFeatures["Coord<Minimum>"]
 lowerRightBBs = regionFeatures["Coord<Maximum>"]
 nBoxes = len(upperLeftBBs)-1
@@ -129,7 +172,10 @@ nBoxes = len(upperLeftBBs)-1
 segments = []
 nonEmptyBoxIndices = []
 for i in range(nBoxes):
-    subImg = labels[ upperLeftBBs[i+1][0]:lowerRightBBs[i+1][0], upperLeftBBs[i+1][1]:lowerRightBBs[i+1][1] ].copy()
+    slices = []
+    for d in range(dim):
+        slices.append(slice(upperLeftBBs[i+1][d], lowerRightBBs[i+1][d]))
+    subImg = labels[slices].copy()
     where = numpy.where(subImg==i+1)
     if len(where[0]) > 0:
         subImg[where] = 0
@@ -141,12 +187,11 @@ for i in range(nBoxes):
 pathLengths = []
 counter = 0
 for seg in segments:
-    #eccentricity(subImg, distFunc="exponential", showPathImage=True, percentageOfPaths=percentage)
-    #eccentricity(subImg, distFunc="inverse", showPathImage=True, percentageOfPaths=percentage)
-    pathLength = eccentricity(seg, distFunc="linear", showPathImage=False, percentageOfPaths=percentage)
+    saveName = "ecc_seg_"+`counter`+".png"
+    #saveName = ""
+    pathLength = eccentricity(seg, distFunc="linear", showPathImage=False, percentageOfPaths=percentage, imgSaveName=saveName)
     pathLengths.append(pathLength)
     counter = counter+1
-    #vigra.show()
 
 # ## Testimage: map longest path to color
 # maxPath = 0
