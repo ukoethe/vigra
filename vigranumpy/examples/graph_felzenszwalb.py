95c348d188e7e481cbba03834d574b2acc42cf38
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2014-03-04 08:55:53 +0100

    minor change in examples

--- vigranumpy/examples/graph_felzenszwalb.py
+++ vigranumpy/examples/graph_felzenszwalb.py
@@ -20,7 +20,7 @@ img          = vigra.impex.readImage(f)#[0:100,0:100,:]
 imgLab       = vigra.colors.transform_RGB2Lab(img)
 imgLabInterpolated  = vigra.resize(imgLab,[imgLab.shape[0]*2-1,imgLab.shape[1]*2-1 ])
 gradmag      = numpy.squeeze(vigra.filters.gaussianGradientMagnitude(imgLabInterpolated,sigma))
-labels ,nseg = vigra.analysis.slicSuperpixels(imgLab,10.0,5)
+labels ,nseg = vigra.analysis.slicSuperpixels(imgLab,10.0,2)
 labels       = numpy.squeeze(vigra.analysis.labelImage(labels))
 
 
@@ -29,21 +29,11 @@ gridGraphEdgeIndicator =  vigraph.edgeFeaturesFromInterpolatedImage(gridGraph,gr
 
 
 
-rag = vigraph.regionAdjacencyGraph(graph=gridGraph,labels=labels,ignoreLabel=0)
-edgeIndicator  = rag.accumulateEdgeFeatures(gridGraphEdgeIndicator)
-hyperNodeSizes = rag.accumulateNodeSize()
-nodeFeatures   = rag.accumulateNodeFeatures(img)
+rag = vigraph.regionAdjacencyGraph(gridGraph,labels)
+edgeWeights = rag.accumulateEdgeFeatures(gridGraphEdgeIndicator)
+labels   = vigraph.felzenszwalbSegmentation(rag,edgeWeights,k=50)
 
 
-
-edgeWeights = edgeIndicator 
-edgeWeights+= 0.1*vigraph.nodeFeatureDistToEdgeWeight(rag,nodeFeatures,metric='l1')
-
-
-ragLabels   = vigraph.felzenszwalbSegmentation(rag,edgeWeights,k=50)
-imageLabels = vigraph.nodeIdsLabels(graph=rag,nodeIds=labels,labels=ragLabels)
-
-
-vigra.segShow(img,imageLabels)
+rag.show(img,labels)
 vigra.show()
 
