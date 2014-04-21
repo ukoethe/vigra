c56b34b48e5ab2ce91b2486ff32726f058de881a
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2014-04-22 00:01:44 +0200

    added a sparse vector

---
+++ include/vigra/sparse_array.hxx
@@ -0,0 +1,306 @@
+/************************************************************************/
+/*                                                                      */
+/*               Copyright 2014 by Thorsten Beier                       */
+/*                                                                      */
+/*    This file is part of the VIGRA computer vision library.           */
+/*    The VIGRA Website is                                              */
+/*        http://hci.iwr.uni-heidelberg.de/vigra/                       */
+/*    Please direct questions, bug reports, and contributions to        */
+/*        ullrich.koethe@iwr.uni-heidelberg.de    or                    */
+/*        vigra@informatik.uni-hamburg.de                               */
+/*                                                                      */
+/*    Permission is hereby granted, free of charge, to any person       */
+/*    obtaining a copy of this software and associated documentation    */
+/*    files (the "Software"), to deal in the Software without           */
+/*    restriction, including without limitation the rights to use,      */
+/*    copy, modify, merge, publish, distribute, sublicense, and/or      */
+/*    sell copies of the Software, and to permit persons to whom the    */
+/*    Software is furnished to do so, subject to the following          */
+/*    conditions:                                                       */
+/*                                                                      */
+/*    The above copyright notice and this permission notice shall be    */
+/*    included in all copies or substantial portions of the             */
+/*    Software.                                                         */
+/*                                                                      */
+/*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND    */
+/*    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES   */
+/*    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND          */
+/*    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT       */
+/*    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,      */
+/*    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      */
+/*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR     */
+/*    OTHER DEALINGS IN THE SOFTWARE.                                   */                
+/*                                                                      */
+/************************************************************************/
+
+#ifndef VIGRA_SPARSE_ARRAY
+#define VIGRA_SPARSE_ARRAY
+
+
+#include <map>
+
+
+namespace vigra{
+
+
+    template<class SA>
+    class SparseReturnProxy{
+    private:
+        typedef typename SA::MapType MapType;
+        typedef typename SA::ConstMapIter ConstMapIter;
+        typedef typename SA::MapIter MapIter;
+        typedef typename SA::coordinate_value_pair coordinate_value_pair;
+
+
+    public:
+        typedef typename SA::value_type value_type;
+        SparseReturnProxy(SA * sa = NULL,const size_t index = 0):
+        sa_(sa),
+        index_(index){
+
+        }
+
+
+        // write access
+        // - convert from value_type to proxy
+        SparseReturnProxy & operator=( const value_type & value ) {
+            sa_->write(index_,value);
+            return *this;
+        }
+
+        // read access
+        // - convert from proxy to value_type
+        operator value_type()const{
+            return sa_->read(index_);
+        }
+
+
+
+
+        SparseReturnProxy<SA> & operator ++ (){
+            MapIter iter = sa_->find(index_);
+            if(iter==sa_->storageEnd()){
+                value_type val(sa_->defaultValue());
+                ++val;
+                if(!sa_->eqComp_(val,sa_->defaultValue()))
+                    sa_->write(index_, val);
+            }
+            else
+                ++iter->second;
+            return *this;
+        }
+        SparseReturnProxy<SA> & operator ++ (int){
+            MapIter iter = sa_->storage_.find(index_);
+            if(iter==sa_->storage_.end()){
+                value_type val(sa_->defaultValue());
+                val++;
+                if(!sa_->eqComp_(val,sa_->defaultValue()))
+                    sa_->write(index_, val);
+            }
+            else
+                iter->second++;
+            return *this;
+        }
+
+
+        SparseReturnProxy<SA> & operator += (const value_type & value){
+            MapIter iter = sa_->storage_.find(index_);
+            if(iter==sa_->storage_.end()){
+                value_type val(sa_->defaultValue());
+                val+=value;
+                if(!sa_->eqComp_(val,sa_->defaultValue()))
+                    sa_->write(index_, val);
+            }
+            else
+                iter->second+=value;
+            return *this;
+        }
+        SparseReturnProxy<SA> & operator -= (const value_type & value){
+            MapIter iter = sa_->storage_.find(index_);
+            if(iter==sa_->storage_.end()){
+                value_type val(sa_->defaultValue());
+                val-=value;
+                if(!sa_->eqComp_(val,sa_->defaultValue()))
+                    sa_->write(index_, val);
+            }
+            else
+                iter->second-=value;
+            return *this;
+        }
+        SparseReturnProxy<SA> & operator *= (const value_type & value){
+            MapIter iter = sa_->storage_.find(index_);
+            if(iter==sa_->storage_.end()){
+                value_type val(sa_->defaultValue());
+                val*=value;
+                if(!sa_->eqComp_(val,sa_->defaultValue()))
+                    sa_->write(index_, val);
+            }
+            else
+                iter->second*=value;
+            return *this;
+        }
+
+        SparseReturnProxy<SA> & operator /= (const value_type & value){
+            MapIter iter = sa_->storage_.find(index_);
+            if(iter==sa_->storage_.end()){
+                value_type val(sa_->defaultValue());
+                val/=value;
+                if(!sa_->eqComp_(val,sa_->defaultValue()))
+                    sa_->write(index_, val);
+            }
+            else
+                iter->second/=value;
+            return *this;
+        }
+
+
+
+    private:
+        SA * sa_;
+        size_t index_;
+    };
+
+
+
+
+
+
+
+
+
+    template<class T ,class EQUAL_COMP = std::equal_to<T> >
+    class SparseMapVector{
+
+
+    private:
+        typedef SparseMapVector<T,EQUAL_COMP> SelfType;
+        typedef SparseReturnProxy<SelfType> Proxy;
+        typedef EQUAL_COMP EqualCompare;
+        friend class SparseReturnProxy<SparseMapVector<T> >;
+
+        typedef std::map<size_t,T> MapType;
+        typedef typename MapType::const_iterator ConstMapIter;
+        typedef typename MapType::iterator MapIter;
+        
+    public:
+        typedef std::pair<size_t,T> coordinate_value_pair;
+        typedef size_t coordinate_type;
+        typedef T value_type;
+        typedef const T & const_reference;
+        typedef Proxy reference;
+
+        SparseMapVector(const size_t size=0, const T & defaultValue  = T(0),
+                        const EqualCompare & eqComp = EqualCompare())
+        :   size_(size),
+            storage_(),
+            defaultValue_(defaultValue),
+            eqComp_(eqComp)
+        {
+
+        }
+
+        template<class INDEX_VALUE_PAIR_ITER>
+        SparseMapVector(const size_t size,INDEX_VALUE_PAIR_ITER indexValuePairBegin,
+                        INDEX_VALUE_PAIR_ITER indexValuePairEnd,
+                        const T & defaultValue  = T(0),
+                        const EqualCompare & eqComp = EqualCompare() )
+        :   size_(size),
+            storage_(indexValuePairBegin,indexValuePairEnd),
+            defaultValue_(defaultValue),
+            eqComp_(eqComp)
+        {
+
+        }
+
+
+        template<class ITER_INDEX , class ITER_VALUE>
+        SparseMapVector(const size_t size, ITER_INDEX indexBegin,
+                        ITER_INDEX indexEnd,ITER_VALUE valueBegin,
+                        const T & defaultValue  = T(0),
+                        const EqualCompare & eqComp = EqualCompare())
+        :   size_(size),
+            storage_(),
+            defaultValue_(defaultValue),
+            eqComp_(eqComp)
+        {
+            const size_t nVals = std::distance(indexBegin,indexEnd);
+            std::vector<coordinate_value_pair> indexValuePairVec(nVals);
+            for(size_t i=0;i<nVals;++i){
+                coordinate_value_pair & mp = indexValuePairVec[i];
+                mp.first=*indexBegin;
+                mp.second=*valueBegin;
+                ++indexBegin;
+                ++valueBegin;
+            }
+            MapType tmp(indexValuePairVec.begin(),indexValuePairVec.end());
+            storage_.swap(tmp);
+        }
+
+
+
+        reference operator()(const size_t index){
+            return Proxy(this,index);
+        }
+
+        reference operator[](const size_t index){
+            return Proxy(this,index);
+        }
+
+
+
+
+        const T & read(const size_t index)const{
+            const ConstMapIter iter = storage_.find(index);
+            return iter==storage_.end() ? defaultValue_ : iter->second;
+        }
+
+        void write(const size_t index,const T & value){
+            std::pair<MapIter,bool> ret=storage_.insert(coordinate_value_pair(index,value));
+            if(ret.second==false){
+                ret.first->second=value;
+            }
+        }
+
+        size_t size()const{
+            return size_;
+        }
+
+        const_reference defaultValue()const{
+            return defaultValue_;
+        }
+
+
+        void  swap(SelfType & other){
+            storage_.swap(other.storage_);
+            std::swap(size_,other.size_);
+            std::swap(defaultValue_,other.defaultValue_);
+            std::swap(eqComp_,other.eqComp_);
+        }
+
+    private:
+
+        MapIter find(const size_t index){
+            return storage_.find(index);
+        }
+        ConstMapIter find(const size_t index)const{
+            return storage_.find(index);
+        }
+
+
+        MapIter storageEnd(){
+            return storage_.end();
+        }
+        ConstMapIter storageEnd()const{
+            return storage_.end();
+        }
+
+
+        size_t size_; 
+        MapType storage_;
+        T defaultValue_;
+        EqualCompare eqComp_;
+    };
+
+} // namespace vigra
+
+#endif /* VIGRA_SPARSE_ARRAY */
---
+++ test/sparse_vector/test.cxx
@@ -0,0 +1,251 @@
+/************************************************************************/
+/*                                                                      */
+/*                 Copyright 2004 by Ullrich Koethe                     */
+/*                                                                      */
+/*    This file is part of the VIGRA computer vision library.           */
+/*    The VIGRA Website is                                              */
+/*        http://hci.iwr.uni-heidelberg.de/vigra/                       */
+/*    Please direct questions, bug reports, and contributions to        */
+/*        ullrich.koethe@iwr.uni-heidelberg.de    or                    */
+/*        vigra@informatik.uni-hamburg.de                               */
+/*                                                                      */
+/*    Permission is hereby granted, free of charge, to any person       */
+/*    obtaining a copy of this software and associated documentation    */
+/*    files (the "Software"), to deal in the Software without           */
+/*    restriction, including without limitation the rights to use,      */
+/*    copy, modify, merge, publish, distribute, sublicense, and/or      */
+/*    sell copies of the Software, and to permit persons to whom the    */
+/*    Software is furnished to do so, subject to the following          */
+/*    conditions:                                                       */
+/*                                                                      */
+/*    The above copyright notice and this permission notice shall be    */
+/*    included in all copies or substantial portions of the             */
+/*    Software.                                                         */
+/*                                                                      */
+/*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND    */
+/*    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES   */
+/*    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND          */
+/*    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT       */
+/*    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,      */
+/*    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      */
+/*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR     */
+/*    OTHER DEALINGS IN THE SOFTWARE.                                   */                
+/*                                                                      */
+/************************************************************************/
+
+#include <iostream>
+#include "vigra/unittest.hxx"
+#include "vigra/sparse_array.hxx"
+
+using namespace vigra;
+
+
+
+int ff1(int a){
+    return a;
+}
+
+
+int ff2(int a,int b){
+    return a+b;
+}
+
+
+
+
+struct SparseVectorTest{   
+
+    typedef SparseMapVector<int> IntVec;
+    typedef IntVec::coordinate_value_pair coordinate_value_pair;
+    void testProxy1(){
+        IntVec vec(100);
+
+        //vec.write(20,25);
+        //shouldEqual(vec.read(20),25);
+
+
+        vec(10)=10;
+        int a;
+        a=vec(10);
+        shouldEqual(a,10);
+
+        vec(11)=11;
+        shouldEqual(vec(11),11);
+
+        vec(12)=11;
+        ++vec(12);
+        shouldEqual(vec(12),12);
+
+        vec(13)=12;
+        vec(13)++;
+        shouldEqual(vec(13),13);
+
+        vec(15)=10;
+        vec(15)+=5;
+        shouldEqual(vec(15),15);
+
+        vec(15)=10;
+        vec(5)=5;
+        shouldEqual(vec(15),10);
+        shouldEqual(vec(5),5);
+        vec(15)+=vec(5);
+        shouldEqual(vec(15),15);
+
+
+        vec(15)-=vec(5);
+        shouldEqual(vec(15),10);
+        vec(15)++;
+        vec(15)++;
+        vec(15)++;
+        vec(15)++;
+        vec(15)++;
+        shouldEqual(vec(15),15);
+
+
+        vec(77)*=vec(77);
+        shouldEqual(vec(77),0);
+
+        vec(77)=2;
+        vec(77)*=vec(77);
+        shouldEqual(vec(77),4);
+    }
+
+    void testProxy2(){
+        IntVec vec(100);
+
+        shouldEqual(vec(10),0);
+        vec(10)++;
+        shouldEqual(vec(10),1);
+
+
+        shouldEqual(vec(11),0);
+        ++vec(11);
+        shouldEqual(vec(11),1);
+
+
+        shouldEqual(vec(12),0);
+        vec(12)+=1;
+        shouldEqual(vec(12),1);
+
+        shouldEqual(vec(13),0);
+        vec(13)-=1;
+        shouldEqual(vec(13),-1);
+
+
+        shouldEqual(vec(14),0);
+        vec(14)*=2;
+        shouldEqual(vec(14),0);
+
+        shouldEqual(vec(14),0);
+        vec(14)/=2;
+        shouldEqual(vec(14),0);
+
+        vec(14)=2;
+        vec(14)*=2;
+        shouldEqual(vec(14),4);
+        vec(14)/=2;
+        shouldEqual(vec(14),2);
+    }
+
+
+    void testProxy3(){
+        IntVec vec(100);
+        int res;
+
+
+        res = vec(20)+vec(10);
+        shouldEqual(res,0);
+
+        vec(20)=2;
+        res = vec(20)+vec(10);
+        shouldEqual(res,2);
+
+        vec(10)=3;
+        res = vec(20)+vec(10);
+        shouldEqual(res,5);
+
+        res = vec(20)*vec(10);
+        shouldEqual(res,6);
+    }
+
+    void testProxy4(){
+        IntVec vec(100);
+
+        std::vector<int> dvec(100,0);
+
+        int res;
+
+
+        res = vec[20]+dvec[20];
+        shouldEqual(res,0);
+
+        vec[20]=2;
+        res = vec[20]+dvec[10];
+        shouldEqual(res,2);
+
+        dvec[10]=3;
+        res =  vec[20]+dvec[10];
+        shouldEqual(res,5);
+
+        res = vec[20]*dvec[10];
+        shouldEqual(res,6);
+    }
+
+
+    void testBatchConstructor1(){
+        int indices[5]={10,20,30,40,50};
+        int values[5]={1,2,3,4,5};
+        IntVec vec(100,indices,indices+5,values);
+        shouldEqual(vec.size(),100);
+        for(size_t i=0;i<5;++i){
+            shouldEqual(vec[indices[i]],values[i]);
+        }
+    }
+    void testBatchConstructor2(){
+        coordinate_value_pair cv[5]={
+            coordinate_value_pair(10,1),
+            coordinate_value_pair(20,2),
+            coordinate_value_pair(30,3),
+            coordinate_value_pair(40,4),
+            coordinate_value_pair(50,5)
+        };
+
+        IntVec vec(100,cv,cv+5);
+        shouldEqual(vec.size(),100);
+        for(size_t i=0;i<5;++i){
+            shouldEqual( vec[cv[i].first], cv[i].second );
+        }
+    }
+};
+
+
+
+
+
+ 
+struct SparseArrayTestSuite
+: public vigra::test_suite
+{
+    SparseArrayTestSuite()
+    : vigra::test_suite("SparseArrayTestSuite")
+    {   
+        add( testCase( &SparseVectorTest::testProxy1));
+        add( testCase( &SparseVectorTest::testProxy2));
+        add( testCase( &SparseVectorTest::testProxy3));
+        add( testCase( &SparseVectorTest::testProxy4));
+        add( testCase( &SparseVectorTest::testBatchConstructor1));
+        add( testCase( &SparseVectorTest::testBatchConstructor2));
+    }
+};
+
+int main(int argc, char ** argv)
+{
+    SparseArrayTestSuite test;
+
+    int failed = test.run(vigra::testsToBeExecuted(argc, argv));
+
+    std::cout << test.report() << std::endl;
+
+    return (failed != 0);
+}
+
