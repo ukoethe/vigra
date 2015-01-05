4a69f59b78a25b8721cc89126e121083c1b92c52
Stuart Berg <bergs@janelia.hhmi.org>
2015-01-05 11:32:56 -0500

    cmake: The presence of std::thread is determined by compiling a test file.

--- config/VigraConfigureThreading.cmake
+++ config/VigraConfigureThreading.cmake
@@ -2,11 +2,20 @@ MACRO(VIGRA_CONFIGURE_THREADING)
     set(THREADING_FOUND 0)
     if(((APPLE AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.7.0") OR MINGW)
          AND NOT WITH_BOOST_THREAD)
-        set(THREADING_FOUND 0)
-        if(${1} MATCHES "REQUIRED")
-            MESSAGE(FATAL_ERROR "Compiler does not support C++ threading. Try to run cmake with '-DWITH_BOOST_THREAD=1'.")
-        else()
-            ADD_DEFINITIONS(-DVIGRA_SINGLE_THREADED)
+        TRY_COMPILE(THREADING_FOUND 
+            ${CMAKE_BINARY_DIR} ${PROJECT_SOURCE_DIR}/config/check_std_thread.cxx
+            OUTPUT_VARIABLE THREADING_TEST_OUTPUT )
+        if (NOT THREADING_FOUND)
+            if(${ARGN} MATCHES "REQUIRED")
+                MESSAGE(FATAL_ERROR "Compiler does not support C++ threading. Try to run cmake with '-DWITH_BOOST_THREAD=1'.\n"
+                "For reference, the failed compiler output is shown below."
+                "${THREADING_TEST_OUTPUT}\n\n"
+                "Compiler does not support C++ threading. Try to run cmake with '-DWITH_BOOST_THREAD=1'.\n"
+                "For reference, the failed compiler output is shown above.\n"
+                )
+            else()
+                ADD_DEFINITIONS(-DVIGRA_SINGLE_THREADED)
+            endif()
         endif()
     else()
         if(WITH_BOOST_THREAD)
@@ -14,7 +23,7 @@ MACRO(VIGRA_CONFIGURE_THREADING)
                 set(THREADING_FOUND 1)
                 INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIR})
                 SET(THREADING_LIBRARIES ${Boost_THREAD_LIBRARY} ${Boost_SYSTEM_LIBRARY} ${Boost_DATE_TIME_LIBRARY} ${Boost_CHRONO_LIBRARY})
-            elseif(${1} MATCHES "REQUIRED")  
+            elseif(${ARGV1} MATCHES "REQUIRED")  
                 MESSAGE(FATAL_ERROR "Cmake was run with '-DWITH_BOOST_THREAD=1', but required boost components were not found.")
             else()
                 ADD_DEFINITIONS(-DVIGRA_SINGLE_THREADED)
