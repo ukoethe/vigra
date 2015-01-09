178865455f6ae50600bdc69263cb663e634de2d4
Stuart Berg <bergs@janelia.hhmi.org>
2015-01-09 11:40:45 -0500

    cmake: Detect C++ version by compiling a test file.

---
+++ config/VigraDetectCppVersion.cmake
@@ -0,0 +1,14 @@
+macro(VIGRA_DETECT_CPP_VERSION)
+
+try_run(RUN_RESULT COMPILE_SUCCEEDED
+        ${CMAKE_BINARY_DIR} ${PROJECT_SOURCE_DIR}/config/output_cplusplus_version.cxx
+        COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT_FROM_CPP_DETECT
+        RUN_OUTPUT_VARIABLE VIGRA_CPP_VERSION)
+
+    if(NOT COMPILE_SUCCEEDED)
+        message(FATAL_ERROR "Failed to detect c++ version with a simple test program!  Compiler Output is below.\n"
+                "${COMPILE_OUTPUT_FROM_CPP_DETECT}")
+    endif()
+    message(STATUS "Detected C++ version: ${VIGRA_CPP_VERSION}")
+
+endmacro(VIGRA_DETECT_CPP_VERSION)
