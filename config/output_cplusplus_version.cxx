0616adb097dfa7642cf6a2a11acf61f53332c774
Stuart Berg <bergs@janelia.hhmi.org>
2015-01-09 16:15:00 -0500

    cmake: Modified c++ detection file to accomodate gcc weirdness.

--- config/output_cplusplus_version.cxx
+++ config/output_cplusplus_version.cxx
@@ -1,2 +1,19 @@
 #include <iostream>
-int main() { std::cout << __cplusplus; return 0; }
+
+int main() {
+    if (__cplusplus == 1)
+    {
+        // This is a workaround for a gcc bug:
+        // http://stackoverflow.com/questions/7530047/gnu-c-macro-cplusplus-standard-conform
+#ifdef __GXX_EXPERIMENTAL_CXX0X__
+        std::cout << 201103;
+#else
+        std::cout << 199711;
+#endif
+    }
+    else
+    {
+        std::cout << __cplusplus;
+    }
+    return 0;
+}
