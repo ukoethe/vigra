37820e64710f96173a665139955d52d3636d88fb
Stuart Berg <bergs@janelia.hhmi.org>
2015-01-09 11:50:10 -0500

    Updates to VIGRA_CONFIGURE_THREADING(): - Detect std::thread support by compiling a test file. - Detect threading implementation ONCE, from root CMakeLists.txt (after boost is detected) - If "REQUIRED" is provided to VIGRA_CONFIGURE_THREADING(), then lack of thread support stops the build (it seems this was previously broken).

--- config/VigraConfigureThreading.cmake
+++ config/VigraConfigureThreading.cmake
@@ -1,34 +1,32 @@
-MACRO(VIGRA_CONFIGURE_THREADING)
-    set(THREADING_FOUND 0)
-    if(((APPLE AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.7.0") OR MINGW)
-         AND NOT WITH_BOOST_THREAD)
-        set(THREADING_FOUND 0)
-        if(${1} MATCHES "REQUIRED")
-            MESSAGE(FATAL_ERROR "Compiler does not support C++ threading. Try to run cmake with '-DWITH_BOOST_THREAD=1'.")
+macro(VIGRA_CONFIGURE_THREADING)
+    get_property(THREADING_IMPLEMENTATION GLOBAL PROPERTY THREADING_IMPLEMENTATION)
+    if(NOT THREADING_IMPLEMENTATION)
+        message(FATAL_ERROR "Threading implementation not detected yet: "
+                            "Your CMakeLists file should not call VIGRA_CONFIGURE_THREADING() until after VigraDetectThreading " 
+                            "has been included from the root CMakeLists.txt file.")
+    endif()
+
+    set(THREADING_FOUND 1)
+    if(THREADING_IMPLEMENTATION MATCHES "boost")
+        include_directories(${Boost_INCLUDE_DIR})
+        set(THREADING_LIBRARIES ${Boost_THREAD_LIBRARY} ${Boost_SYSTEM_LIBRARY} ${Boost_DATE_TIME_LIBRARY} ${Boost_CHRONO_LIBRARY})
+    elseif(THREADING_IMPLEMENTATION MATCHES "std")
+        # Great, we can use the std library.  Nothing to do here...
+    elseif(THREADING_IMPLEMENTATION MATCHES "none")
+        if("${ARGN}" MATCHES "REQUIRED")
+            message(FATAL_ERROR 
+                "***********************************************\n"
+                "${THREADING_TEST_OUTPUT}\n"
+                "***********************************************\n"
+                "C++ threading is required, but your compiler doesn't support it. \n"
+                "For reference, the failed compiler output is shown above. \n"
+                "Try to run cmake with '-DWITH_BOOST_THREAD=1'.\n")
         else()
-            ADD_DEFINITIONS(-DVIGRA_SINGLE_THREADED)
+            message("(Compiling single threaded, consider running 'cmake -DWITH_BOOST_THREAD=1')")
+            add_definitions(-DVIGRA_SINGLE_THREADED)
+            set(THREADING_FOUND 0)
         endif()
     else()
-        if(WITH_BOOST_THREAD)
-            if(Boost_FOUND)
-                set(THREADING_FOUND 1)
-                INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIR})
-                SET(THREADING_LIBRARIES ${Boost_THREAD_LIBRARY} ${Boost_SYSTEM_LIBRARY} ${Boost_DATE_TIME_LIBRARY} ${Boost_CHRONO_LIBRARY})
-            elseif(${1} MATCHES "REQUIRED")  
-                MESSAGE(FATAL_ERROR "Cmake was run with '-DWITH_BOOST_THREAD=1', but required boost components were not found.")
-            else()
-                ADD_DEFINITIONS(-DVIGRA_SINGLE_THREADED)
-            endif()
-        else()
-            # note: we only support MSVC versions that have threading
-            set(THREADING_FOUND 1)
-            IF (CMAKE_COMPILER_IS_GNUCXX AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.7.0")
-                SET(CMAKE_CXX_FLAGS "-pthread -std=c++0x ${CMAKE_CXX_FLAGS}")
-            elseif(CMAKE_COMPILER_IS_GNUCXX)
-                SET(CMAKE_CXX_FLAGS "-pthread -std=c++11 ${CMAKE_CXX_FLAGS}")
-            elseif(NOT MSVC)
-                SET(CMAKE_CXX_FLAGS "-std=c++11 ${CMAKE_CXX_FLAGS}")
-            endif()
-        endif()
+        message(FATAL_ERROR "CMakeLists bug: Unknown threading implementation")
     endif()
-ENDMACRO(VIGRA_CONFIGURE_THREADING)
+endmacro(VIGRA_CONFIGURE_THREADING)
