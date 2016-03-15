55652337d5836fc23e614daa64fe39ae3851adde
Philip Schill <philip.schill@gmx.de>
2016-03-15 12:41:33 +0100

    Applied vigra code conventions to rf import/export.

--- include/vigra/random_forest_new_hdf5_impex.hxx
+++ include/vigra/random_forest_new_hdf5_impex.hxx
@@ -58,148 +58,143 @@ static const char *const rf_hdf5_version_group = ".";
 static const char *const rf_hdf5_version_tag   = "vigra_random_forest_version";
 static const double      rf_hdf5_version       =  0.1;
 
-
 // keep in sync with include/vigra/random_forest/rf_nodeproxy.hxx
 enum NodeTags
 {
-	rf_UnFilledNode        = 42,
-	rf_AllColumns          = 0x00000000,
-	rf_ToBePrunedTag       = 0x80000000,
-	rf_LeafNodeTag         = 0x40000000,
+    rf_UnFilledNode        = 42,
+    rf_AllColumns          = 0x00000000,
+    rf_ToBePrunedTag       = 0x80000000,
+    rf_LeafNodeTag         = 0x40000000,
 
-	rf_i_ThresholdNode     = 0,
-	rf_i_HyperplaneNode    = 1,
-	rf_i_HypersphereNode   = 2,
-	rf_e_ConstProbNode     = 0 | rf_LeafNodeTag,
-	rf_e_LogRegProbNode    = 1 | rf_LeafNodeTag
+    rf_i_ThresholdNode     = 0,
+    rf_i_HyperplaneNode    = 1,
+    rf_i_HypersphereNode   = 2,
+    rf_e_ConstProbNode     = 0 | rf_LeafNodeTag,
+    rf_e_LogRegProbNode    = 1 | rf_LeafNodeTag
 };
 
 static const unsigned int rf_tag_mask = 0xf0000000;
 static const unsigned int rf_type_mask = 0x00000003;
 static const unsigned int rf_zero_mask = 0xffffffff & ~rf_tag_mask & ~rf_type_mask;
 
-
 template <typename FEATURES, typename LABELS>
 typename DefaultRF<FEATURES, LABELS>::type
-random_forest_import_HDF5(HDF5File &h5ctx, const std::string &pathname = "")
+random_forest_import_HDF5(HDF5File & h5ctx, std::string const & pathname = "")
 {
-	typedef typename DefaultRF<FEATURES, LABELS>::type RF;
-	typedef typename RF::Graph Graph;
-	typedef typename RF::Node Node;
-	typedef typename RF::SplitTests SplitTest;
-	typedef typename LABELS::value_type LabelsType;
-	typedef typename RF::AccInputType::value_type AccValueType;
+    typedef typename DefaultRF<FEATURES, LABELS>::type RF;
+    typedef typename RF::Graph Graph;
+    typedef typename RF::Node Node;
+    typedef typename RF::SplitTests SplitTest;
+    typedef typename LABELS::value_type LabelType;
+    typedef typename RF::AccInputType AccInputType;
+    typedef typename AccInputType::value_type AccValueType;
 
-	std::string cwd;
+    std::string cwd;
 
-	if (pathname.size()) {
-		cwd = h5ctx.get_absolute_path(h5ctx.pwd());
-		h5ctx.cd(pathname);
-	}
+    if (pathname.size()) {
+        cwd = h5ctx.get_absolute_path(h5ctx.pwd());
+        h5ctx.cd(pathname);
+    }
 
-	if (h5ctx.existsAttribute(rf_hdf5_version_group, rf_hdf5_version_tag)) {
-		double version;
-		h5ctx.readAttribute(rf_hdf5_version_group, rf_hdf5_version_tag, version);
-		vigra_precondition(version <= rf_hdf5_version, "random_forest_import_HDF5(): unexpected file format version.");
-	}
+    if (h5ctx.existsAttribute(rf_hdf5_version_group, rf_hdf5_version_tag)) {
+        double version;
+        h5ctx.readAttribute(rf_hdf5_version_group, rf_hdf5_version_tag, version);
+        vigra_precondition(version <= rf_hdf5_version, "random_forest_import_HDF5(): unexpected file format version.");
+    }
 
+    size_t mtry;
+    size_t num_instances;
+    size_t num_features;
+    size_t num_classes;
+    size_t num_trees;
+    MultiArray<1, LabelType> distinct_labels_marray;
 
-	size_t mtry;
-	size_t num_instances;
-	size_t num_features;
-	size_t num_classes;
-	size_t num_trees;
-	MultiArray<1, LabelsType> distinct_labels_marray;
-	std::vector<LabelsType> distinct_labels;
+    h5ctx.read("/_ext_param/actual_mtry_", mtry);
+    h5ctx.read("/_ext_param/row_count_", num_instances);
+    h5ctx.read("/_ext_param/column_count_", num_features);
+    h5ctx.read("/_ext_param/class_count_", num_classes);
+    h5ctx.read("/_options/tree_count_", num_trees);
+    h5ctx.readAndResize("/_ext_param/labels", distinct_labels_marray);
 
-	h5ctx.read("/_ext_param/actual_mtry_", mtry);
-	h5ctx.read("/_ext_param/row_count_", num_instances);
-	h5ctx.read("/_ext_param/column_count_", num_features);
-	h5ctx.read("/_ext_param/class_count_", num_classes);
-	h5ctx.read("/_options/tree_count_", num_trees);
-	h5ctx.readAndResize("/_ext_param/labels", distinct_labels_marray);
+    std::vector<LabelType> distinct_labels(distinct_labels_marray.begin(), distinct_labels_marray.end());
 
-	for (auto i = distinct_labels_marray.begin(); i != distinct_labels_marray.end(); ++i) {
-		distinct_labels.push_back(*i);
-	}
+    auto const pspec = ProblemSpecNew<LabelType>()
+                               .num_instances(num_instances)
+                               .actual_mtry(mtry)
+                               .num_features(num_features)
+                               .distinct_classes(distinct_labels);
 
-	auto const pspec = ProblemSpecNew<typename LABELS::value_type>().num_instances(num_instances).actual_mtry(mtry).num_features(num_features).distinct_classes(distinct_labels);
+    Graph gr;
+    typename RF::NodeMap<SplitTest> split_tests;
+    typename RF::NodeMap<AccInputType> leaf_responses;
 
-	Graph gr;
-	PropertyMap<Node, SplitTest, RF::container_tag> split_tests;
-	PropertyMap<Node, typename RF::AccInputType, RF::container_tag> leaf_responses;
+    auto groups = h5ctx.ls();
+    for (auto const & groupname : groups)
+        if (groupname.substr(0, std::char_traits<char>::length(rf_hdf5_tree)).compare(rf_hdf5_tree) != 0) {
+            continue;
+        }
 
-	auto groups = h5ctx.ls();
-	for (auto i = groups.begin(); i != groups.end(); ++i) {
-		std::string groupname = *i;
-		if (groupname.substr(0, std::char_traits<char>::length(rf_hdf5_tree)).compare(rf_hdf5_tree) != 0) {
-			continue;
-		}
+        MultiArray<1, unsigned int> topology;
+        MultiArray<1, double> parameters;
+        h5ctx.cd(groupname);
+        h5ctx.readAndResize("topology", topology);
+        h5ctx.readAndResize("parameters", parameters);
+        h5ctx.cd_up();
 
-		MultiArray<1, unsigned int> topology;
-		MultiArray<1, double> parameters;
-		h5ctx.cd(*i);
-		h5ctx.readAndResize("topology", topology);
-		h5ctx.readAndResize("parameters", parameters);
-		h5ctx.cd_up();
+        vigra_precondition(topology[0] == num_features, "random_forest_import_HDF5(): number of features mismatch.");
+        vigra_precondition(topology[1] == num_classes, "random_forest_import_HDF5(): number of classes mismatch.");
 
-		vigra_precondition(topology[0] == num_features, "random_forest_import_HDF5(): number of features mismatch.");
-		vigra_precondition(topology[1] == num_classes, "random_forest_import_HDF5(): number of classes mismatch.");
+        Node n = gr.addNode();
+        unsigned int index = 2;
 
-		Node n = gr.addNode();
-		unsigned int index = 2;
+        std::queue<std::pair<unsigned int, Node> > q;
+        q.emplace(2, n);
+        while (!q.empty()) {
+            auto el = q.front();
 
-		typedef std::pair<unsigned int, Node> QueuePair;
-		std::queue<QueuePair> q;
-		q.push(std::make_pair(2, n));
+            index = el.first;
+            Node parent = el.second;
 
-		while (!q.empty()) {
-			QueuePair el = q.front();
+            vigra_precondition((topology[index] & rf_zero_mask) == 0, "random_forest_import_HDF5(): unexpected node type: type & zero_mask > 0");
 
-			index = el.first;
-			Node parent = el.second;
+            if (topology[index] & rf_LeafNodeTag) {
+                unsigned int probs_start = topology[index+1] + 1;
 
-			vigra_precondition((topology[index] & rf_zero_mask) == 0, "random_forest_import_HDF5(): unexpected node type: type & zero_mask > 0");
+                vigra_precondition((topology[index] & rf_tag_mask) == rf_LeafNodeTag, "random_forest_import_HDF5(): unexpected node type: additional tags in leaf node");
 
-			if (topology[index] & rf_LeafNodeTag) {
-				unsigned int probs_start = topology[index+1] + 1;
+                std::vector<AccValueType> node_response;
 
-				vigra_precondition((topology[index] & rf_tag_mask) == rf_LeafNodeTag, "random_forest_import_HDF5(): unexpected node type: additional tags in leaf node");
+                for (unsigned int i = 0; i < num_classes; ++i) {
+                    node_response.push_back(parameters[probs_start + i]);
+                }
 
-				std::vector<AccValueType> node_response;
+                leaf_responses.insert(parent, node_response);
 
-				for (unsigned int i = 0; i < num_classes; ++i) {
-					node_response.push_back(parameters[probs_start + i]);
-				}
+            } else {
+                vigra_precondition(topology[index] == rf_i_ThresholdNode, "random_forest_import_HDF5(): unexpected node type.");
 
-				leaf_responses.insert(parent, node_response);
+                Node left = gr.addNode();
+                Node right = gr.addNode();
 
-			} else {
-				vigra_precondition(topology[index] == rf_i_ThresholdNode, "random_forest_import_HDF5(): unexpected node type.");
+                gr.addArc(parent, left);
+                gr.addArc(parent, right);
 
-				Node left = gr.addNode();
-				Node right = gr.addNode();
+                split_tests.insert(parent, SplitTest(topology[index+4], parameters[topology[index+1]+1]));
 
-				gr.addArc(parent, left);
-				gr.addArc(parent, right);
+                q.push(std::make_pair(topology[index+2], left));
+                q.push(std::make_pair(topology[index+3], right));
+            }
 
-				split_tests.insert(parent, SplitTest(topology[index+4], parameters[topology[index+1]+1]));
-
-				q.push(std::make_pair(topology[index+2], left));
-				q.push(std::make_pair(topology[index+3], right));
-			}
-
-			q.pop();
-		}
-	}
+            q.pop();
+        }
+    }
 
 
-	if (cwd.size()) {
-		h5ctx.cd(cwd);
-	}
+    if (cwd.size()) {
+        h5ctx.cd(cwd);
+    }
 
-	RF rf = RF(gr, split_tests, leaf_responses, pspec);
-	return rf;
+    return RF(gr, split_tests, leaf_responses, pspec);
 }
 
 
