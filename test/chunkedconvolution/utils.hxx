13ad82d465344e104d5a1f4adfd4be20b0b9878d
Kevin Kiefer <abc.kiefer@gmail.com>
2016-03-31 11:52:32 +0200

    Overloaded convolution functions for chunked arrays
    new file:   multi_chunked.hxx
    new file:   ../../test/chunkedconvolution/CMakeLists.txt
    new file:   ../../test/chunkedconvolution/test_chunked_convolution.cxx
    new file:   ../../test/chunkedconvolution/utils.hxx
    
    modified:   multi_array_chunked.hxx
    modified:   ../../test/CMakeLists.txt
    
    ** in file multi_chunked.hxx **
    The following functions have been overloaded:
        gaussianSmoothMultiArray
        gaussianGradientMultiArray
        gaussianDivergenceMultiArray
        gaussianGradientMagnitudeMultiArray
        hessianOfGaussianMultiArray
        hessianOfGaussianEigenvaluesMultiArray
        hessianOfGaussianFirstEigenvalueMultiArray
        hessianOfGaussianLastEigenvalueMultiArray
        laplacianOfGaussianMultiArray
        symmetricGradientMultiArray
        structureTensorMultiArray
    
    ** in file multi_array_chunked.hxx **
    Added the "function":
        CompareChunkedArray
    
    ** in folder test **
    Added test for the overloaded functions in multi_chunked.hxx

---
+++ test/chunkedconvolution/utils.hxx
@@ -0,0 +1,51 @@
+/************************************************************************/
+/*                                                                      */
+/*     Copyright 2016 by Ullrich Koethe and Kevin Kiefer                */
+/*                                                                      */
+/*    This file is part of the VIGRA computer vision library.           */
+/*    The VIGRA Website is                                              */
+/*        http://hci.iwr.uni-heidelberg.de/vigra/                       */
+/*    Please direct questions, bug reports, and contributions to        */
+/*        ullrich.koethe@iwr.uni-heidelberg.de    or                    */
+/*        vigra@informatik.uni-hamburg.de                               */
+/*                                                                      */
+/*    Permission is hereby granted, free of charge, to any person       */
+/*    obtaining a copy of this software and associated documentation    */
+/*    files (the "Software"), to deal in the Software without           */
+/*    restriction, including without limitation the rights to use,      */
+/*    copy, modify, merge, publish, distribute, sublicense, and/or      */
+/*    sell copies of the Software, and to permit persons to whom the    */
+/*    Software is furnished to do so, subject to the following          */
+/*    conditions:                                                       */
+/*                                                                      */
+/*    The above copyright notice and this permission notice shall be    */
+/*    included in all copies or substantial portions of the             */
+/*    Software.                                                         */
+/*                                                                      */
+/*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND    */
+/*    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES   */
+/*    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND          */
+/*    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT       */
+/*    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,      */
+/*    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      */
+/*    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR     */
+/*    OTHER DEALINGS IN THE SOFTWARE.                                   */
+/*                                                                      */
+/************************************************************************/
+
+#include <cstdlib>
+#include <vigra/unittest.hxx>
+
+template <typename Iter>
+void fillRandom(Iter begin, Iter end, int max)
+{
+    for (; begin != end; ++begin)
+        *begin = rand() % max;
+}
+
+template <typename Iter1, typename Iter2>
+void compare(Iter1 b1, Iter1 e1, Iter2 b2)
+{
+    for (; b1 != e1; ++b1, ++b2)
+        shouldEqual(*b1, *b2);
+}
