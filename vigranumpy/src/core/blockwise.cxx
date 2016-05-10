1bd775f9f2f1134fbb023d5f80ef20ac69f912f4
Kevin Kiefer <abc.kiefer@gmail.com>
2016-05-10 13:43:58 +0200

    cleanup

--- vigranumpy/src/core/blockwise.cxx
+++ vigranumpy/src/core/blockwise.cxx
@@ -412,8 +412,8 @@ BOOST_PYTHON_MODULE_INIT(blockwise)
     defineUnionFindWatershedsImpl<2, npy_uint32, npy_uint32>();
     defineUnionFindWatershedsImpl<3, npy_uint32, npy_uint32>();
 
-    defineLabelArrayImpl<2, npy_uint32, npy_uint32>();
-    defineLabelArrayImpl<3, npy_uint32, npy_uint32>();
+    defineLabelArrayImpl<2, npy_uint8, npy_uint32>();
+    defineLabelArrayImpl<3, npy_uint8, npy_uint32>();
     defineLabelArrayImpl<2, npy_uint32, npy_uint32>();
     defineLabelArrayImpl<3, npy_uint32, npy_uint32>();
 }
