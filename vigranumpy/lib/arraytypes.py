ukoethe/vigra

commit c69e2f1abb32324b03f861316d418aa4642b32d4

fix for: zmq only takes ascii, no unicode
some divisionfixes / -> //

vigranumpy/lib/arraytypes.py
@@ -45,6 +45,7 @@ import vigra.vigranumpycore as vigranumpycore
 from vigra.vigranumpycore import AxisType, AxisInfo, AxisTags
 
 if sys.version_info[0] > 2:
+    buffer = memoryview
     def xrange(*args):
         return range(*args)
 
@@ -642,7 +643,7 @@ class VigraArray(numpy.ndarray):
             permutation = self.permutationFromNumpyOrder()
         )
         socket.send_json(metadata, flags|zmq.SNDMORE)
-        socket.send(self.axistags.toJSON(), flags|zmq.SNDMORE)
+        socket.send(self.axistags.toJSON().encode('ascii'), flags|zmq.SNDMORE)
         return socket.send(transposed, flags, copy=copy, track=track)
 
     def imshow(self):
@@ -776,7 +777,7 @@ class VigraArray(numpy.ndarray):
                 clip = False
             if m == M:
                 return res
-            f = 255.0 / (M - m)
+            f = 255.0 // (M - m)
             img = f * (img - m)
             if clip:
                 img = numpy.minimum(255.0, numpy.maximum(0.0, img))
@@ -2118,7 +2119,7 @@ class ImagePyramid(list):
         if level > self.highestLevel:
             image = list.__getitem__(self, -1)
             for i in range(self.highestLevel, level):
-                newShape = [int((k + 1) / 2) for k in image.shape]
+                newShape = [int((k + 1) // 2) for k in image.shape]
                 if hasChannels:
                     newShape[channelIndex] = image.shape[channelIndex]
                 if axistags:

commit c1f76acf175d521613a315e3257e836ba8d4e5cb

fix intensity normalization in asRGB
bug introduced in commit c69e2f1abb32324b03f861316d418aa4642b32d4
flooring division produces 0 if scaling factor is <1

vigranumpy/lib/arraytypes.py
@@ -782,7 +782,7 @@ class VigraArray(numpy.ndarray):
                 clip = False
             if m == M:
                 return res
-            f = 255.0 // (M - m)
+            f = 255.0 / (M - m)
             img = f * (img - m)
             if clip:
                 img = numpy.minimum(255.0, numpy.maximum(0.0, img))