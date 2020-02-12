d0d51836214f7961d9da526a76224e704c5367ca
Attila Oláh <atl@google.com>
2020-02-12 01:03:32 +0100

    Fix C++ version detection for WebAssembly.
    The Emscripten compiler (`em++`) will refuse to output the results of the buffer unless they are flushed explicitly, or implicitly with the `std::endl`. This will result in the C++ version being detected as being the literal string:
    
    > `stdio streams had content in them that was not flushed. you should set EXIT_RUNTIME to 1 (see the FAQ), or make sure to emit a newline when you printf etc.`

--- config/output_cplusplus_version.cxx
+++ config/output_cplusplus_version.cxx
@@ -6,14 +6,14 @@ int main() {
         // This is a workaround for a gcc bug:
         // http://stackoverflow.com/questions/7530047/gnu-c-macro-cplusplus-standard-conform
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
-        std::cout << 201103;
+        std::cout << 201103 << std::endl;
 #else
-        std::cout << 199711;
+        std::cout << 199711 << std::endl;
 #endif
     }
     else
     {
-        std::cout << __cplusplus;
+        std::cout << __cplusplus << std::endl;
     }
     return 0;
 }
