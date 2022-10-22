63a68cfb6a28575d38f3ec5e953c8c89dfe68f10
Mark Harfouche <mark.harfouche@gmail.com>
2022-10-22 13:34:20 -0400

    Fix linux CI as a starting point

---
+++ ci/build_unix.sh
@@ -0,0 +1,34 @@
+set -ex
+
+PYTHON_VERSION="${PYTHON_VERSION:-3.10}"
+
+conda config --add channels conda-forge
+conda config --remove channels defaults || true
+conda config --show
+conda create \
+    --quiet --yes \
+    --name vigra \
+    python=${PYTHON_VERSION} c-compiler cxx-compiler \
+    zlib jpeg libpng libtiff hdf5 fftw \
+    boost boost-cpp numpy h5py nose sphinx \
+    openexr lemon
+
+source $CONDA/bin/activate vigra
+
+mkdir build
+cd build
+
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
+    -DCMAKE_PREFIX_PATH=${CONDA_PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPython_ROOT_DIR="${CONDA_PREFIX}" \
+    -DPython_FIND_VIRTUALENV=ONLY \
+    -DTEST_VIGRANUMPY=ON \
+    -DWITH_OPENEXR=ON \
+    -DWITH_LEMON=ON \
+    -DAUTOEXEC_TESTS=OFF
+
+make -j2
+make check -j2
+ctest -V
---
+++ ci/build_windows.bat
@@ -0,0 +1,50 @@
+@echo on
+conda info
+if errorlevel 1 exit 1
+
+conda config --add channels conda-forge
+if errorlevel 1 exit 1
+conda config --remove channels defaults || true
+if errorlevel 1 exit 1
+conda config --show
+if errorlevel 1 exit 1
+
+conda create ^
+    --quiet --yes ^
+    --name vigra ^
+    python=%PYTHON_VERSION% c-compiler cxx-compiler ^
+    zlib jpeg libpng libtiff hdf5 fftw ^
+    boost boost-cpp numpy h5py nose sphinx ^
+    openexr lemon
+if errorlevel 1 exit 1
+
+call activate vigra
+if errorlevel 1 exit 1
+
+mkdir build
+if errorlevel 1 exit 1
+
+cd build
+
+cmake .. ^
+    -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX% ^
+    -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DPython_ROOT_DIR="%CONDA_PREFIX%" ^
+    -DPython_FIND_VIRTUALENV=ONLY ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DTEST_VIGRANUMPY=ON ^
+    -DWITH_OPENEXR=ON ^
+    -DWITH_LEMON=ON ^
+    -DBUILD_TESTS=OFF
+
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+:REM nmake check
+:REM if errorlevel 1 exit 1
+:REM
+:REM ctest -V
+:REM if errorlevel 1 exit 1
