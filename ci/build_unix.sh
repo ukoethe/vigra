6a92f41dfefee356be91676b2f8c1431d8680d59
Mark Harfouche <mark.harfouche@gmail.com>
2022-10-23 21:18:14 -0400

    Use FIND_PACKAGE specify FRAMEWORK=LAST

--- ci/build_unix.sh
+++ ci/build_unix.sh
@@ -2,6 +2,7 @@ set -ex
 
 PYTHON_VERSION="${PYTHON_VERSION:-3.10}"
 
+
 conda config --add channels conda-forge
 conda config --remove channels defaults || true
 conda config --show
@@ -15,9 +16,18 @@ conda create \
 
 source $CONDA/bin/activate vigra
 
+
+if [ ! -d "${CONDA_PREFIX}/include/libpng" ]; then
+    # CMake FindPNG seems to look in libpng not libpng16
+    # https://gitlab.kitware.com/cmake/cmake/blob/master/Modules/FindPNG.cmake#L55
+    ln -s ${CONDA_PREFIX}/include/libpng16 ${CONDA_PREFIX}/include/libpng
+fi
+
 mkdir build
 cd build
 
+# Use CMAKE_FIND_FRAMEWORK=LAST to ensure that
+# we don't find outdated OSX libraries on OSX
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
     -DCMAKE_PREFIX_PATH=${CONDA_PREFIX} \
@@ -27,7 +37,9 @@ cmake .. \
     -DTEST_VIGRANUMPY=ON \
     -DWITH_OPENEXR=ON \
     -DWITH_LEMON=ON \
-    -DAUTOEXEC_TESTS=OFF
+    -DAUTOEXEC_TESTS=OFF \
+    -DCMAKE_FIND_FRAMEWORK=LAST \
+    -DPNG_PNG_INCLUDE_DIR=${CONDA_PREFIX}/include
 
 make -j2
 make check -j2
