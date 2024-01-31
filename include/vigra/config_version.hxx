97744d3f2a41c223a28bf05c28d579c508ab656e
Mark Harfouche <mark.harfouche@gmail.com>
2024-01-31 07:14:26 -0500

    Update the version to 1.11.2

--- include/vigra/config_version.hxx
+++ include/vigra/config_version.hxx
@@ -39,7 +39,7 @@
 
     #define VIGRA_VERSION_MAJOR 1
     #define VIGRA_VERSION_MINOR 11
-    #define VIGRA_VERSION_PATCH 1
-    #define VIGRA_VERSION "1.11.1"
+    #define VIGRA_VERSION_PATCH 2
+    #define VIGRA_VERSION "1.11.2"
 
 #endif /* VIGRA_CONFIG_VERSION_HXX */
