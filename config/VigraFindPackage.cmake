db6b05623baf75b4a5403657628108b1640441cb
Mark Harfouche <mark.harfouche@gmail.com>
2024-08-10 16:50:08 -0400

    Fix FindBoost for modern Boost 1.70 and above (2019)

--- config/VigraFindPackage.cmake
+++ config/VigraFindPackage.cmake
@@ -2,10 +2,10 @@
 #
 #  VIGRA_FIND_PACKAGE(package [VERSION_SPEC] [COMPONENTS component1 ... ] [NAMES name1 ...])
 #
-# FIND_PACKAGE is dangerous when custom search prefixes are added, because these 
+# FIND_PACKAGE is dangerous when custom search prefixes are added, because these
 # prefixes are tried for includes and libraries independently. Thus, it may happen
-# that a library is found under one prefix, whereas the corresponding include is 
-# found under another. Subsequent crashes of the compiled programs are likely. 
+# that a library is found under one prefix, whereas the corresponding include is
+# found under another. Subsequent crashes of the compiled programs are likely.
 #
 # VIGRA_FIND_PACKAGE improves upon this by trying custom prefixes one at a time,
 # so that either both includes and libraries are found under the same prefix,
@@ -32,52 +32,22 @@ MACRO(VIGRA_FIND_PACKAGE package)
     IF(DEFINED COMPONENTS)
         SET(COMPONENTS COMPONENTS ${COMPONENTS})
     ENDIF()
-    
+
     if(VERSION_SPEC)
         set(VERSION_MESSAGE " (at least version ${VERSION_SPEC})")
     else()
         set(VERSION_MESSAGE)
     endif()
-    
+
     MESSAGE(STATUS "Searching for ${package}${VERSION_MESSAGE}")
-    
-    foreach(path ${DEPENDENCY_SEARCH_PREFIX})
-        if(NOT ${package}_FOUND)
-            IF(${package} STREQUAL "Boost")
-                IF(EXISTS "${path}/boost/config.hpp")
-                    SET(BOOST_INCLUDEDIR ${path}) # boost's default include path
-                ELSE()
-                    SET(BOOST_INCLUDEDIR ${path}/include) # standard include path
-                ENDIF()
-                IF(EXISTS "${path}/stage/lib")
-                    SET(BOOST_LIBRARYDIR ${path}/stage/lib) # boost's default library path
-                ELSE()
-                    SET(BOOST_LIBRARYDIR ${path}/lib) # standard library path
-                ENDIF()
-            ELSE()
-                SET(CMAKE_INCLUDE_PATH ${path}/include)
-                SET(CMAKE_LIBRARY_PATH ${path}/lib)
-            ENDIF()
-            # SET(${package}_INCLUDE_DIR ${package}_INCLUDE_DIR-NOTFOUND)
-            # SET(${package}_LIBRARIES ${package}_LIBRARIES-NOTFOUND)
-            # SET(${package}_LIBRARY ${package}_LIBRARY-NOTFOUND)
-            MESSAGE(STATUS "   in prefix ${path}")
-            FIND_PACKAGE(${package} ${VERSION_SPEC} ${COMPONENTS})
-            IF(${package} STREQUAL "Boost")
-                SET(BOOST_INCLUDEDIR)
-                SET(BOOST_LIBRARYDIR)
-            ELSE()
-                SET(CMAKE_INCLUDE_PATH)
-                SET(CMAKE_LIBRARY_PATH)
-            ENDIF()
-        endif()
-    endforeach(path)
-   
-    # search the package in the default locations if not found 
-    # in the DEPENDENCY_SEARCH_PREFIX 
+
+    FIND_PACKAGE(${package} ${VERSION_SPEC} ${COMPONENTS})
+
+    # search the package in the default locations if not found
+    # in the DEPENDENCY_SEARCH_PREFIX
     if(NOT ${package}_FOUND)
         MESSAGE(STATUS "   in default locations")
         FIND_PACKAGE(${package} ${VERSION_SPEC} ${COMPONENTS})
     endif()
-    
-ENDMACRO(VIGRA_FIND_PACKAGE)    
+
+ENDMACRO(VIGRA_FIND_PACKAGE)
