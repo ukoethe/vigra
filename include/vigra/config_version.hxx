5946bac7a9a15542c2612311a280038d71c358f2
Mark Harfouche <mark.harfouche@gmail.com>
2025-12-23 12:07:24 -0500

    Bump version to 1.12.3

--- include/vigra/config_version.hxx
+++ include/vigra/config_version.hxx
@@ -39,7 +39,7 @@
 
     #define VIGRA_VERSION_MAJOR 1
     #define VIGRA_VERSION_MINOR 12
-    #define VIGRA_VERSION_PATCH 2
-    #define VIGRA_VERSION "1.12.2"
+    #define VIGRA_VERSION_PATCH 3
+    #define VIGRA_VERSION "1.12.3"
 
 #endif /* VIGRA_CONFIG_VERSION_HXX */
