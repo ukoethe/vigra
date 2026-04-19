575e101dc4c15cb334244426dbc9eee7ab260af7
Mark Harfouche <mark.harfouche@gmail.com>
2026-04-19 16:29:36 -0400

    Try to fix warnings for C++17

--- include/vigra/functortraits.hxx
+++ include/vigra/functortraits.hxx
@@ -194,7 +194,7 @@ VIGRA_DEFINE_STL_FUNCTOR(std::logical_or, VigraFalseType, VigraTrueType)
 VIGRA_DEFINE_STL_FUNCTOR(std::negate, VigraTrueType, VigraFalseType)
 VIGRA_DEFINE_STL_FUNCTOR(std::logical_not, VigraTrueType, VigraFalseType)
 
-#if !( (__cplusplus >= 202002L) || (defined(_MSC_VER) && _MSVC_LANG >= 202002L) )
+#if !( (__cplusplus >= 201703L) || (defined(_MSC_VER) && _MSVC_LANG >= 201703L) )
 VIGRA_DEFINE_STL_FUNCTOR(std::binary_negate, VigraFalseType, VigraTrueType)
 VIGRA_DEFINE_STL_FUNCTOR(std::unary_negate, VigraTrueType, VigraFalseType)
 #endif
